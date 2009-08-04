2fc3404146dee5f4ab6df7d0269d091fee15b147
andrewsumner <andrewsumner@30fe4595-0a0c-4342-8851-515496e4dcbd>
2009-08-04 04:07:46 +0000

    hymie_sc (1 and 2) deleted
    git-svn-id: https://svn.code.sf.net/p/speed-dreams/code/trunk@1242 30fe4595-0a0c-4342-8851-515496e4dcbd
    
    Former-commit-id: 0000b287758d8fb95275bb07cac7b09511c47797
    Former-commit-id: fbd6fa5bc7bb363a48e88fdffada0f0fdb71d6a7

--- src/drivers/hymie_sc2/name.h
+++
@@ -1,9 +0,0 @@
-#ifndef _DRIVERNAME_H
-#define _DRIVERNAME_H
-
-#define DRIVERNAME "hymie_sc2"
-#define DRIVERNAME1 "hymie 2006 1"
-#define DRIVERNAME2 "hymie 2006 2"
-#define TEAMNAME "hymie"
-
-#endif
