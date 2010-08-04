eac55cb1dc1317dc11c51e6c8546ef4902a5c84a
pouillot <pouillot@30fe4595-0a0c-4342-8851-515496e4dcbd>
2010-08-04 08:23:55 +0000

    Code cleanup
    git-svn-id: https://svn.code.sf.net/p/speed-dreams/code/trunk@2602 30fe4595-0a0c-4342-8851-515496e4dcbd
    
    Former-commit-id: cf17660ae79ac992a0e98e9d50e391b8d52d3efc
    Former-commit-id: d88c45b9eb4096ee1e5c51863b5f35c8d0ca6e72

--- src/windows/osspec.h
+++ src/windows/osspec.h
@@ -30,27 +30,15 @@
 #error Hey ! Where is _WIN32 ??
 #endif
 
-//#include <windows.h>
 #include <cstring>
 #ifdef WIN32
-//#	pragma warning( disable : 4514 4100 4130 4611 4115 4189 4505 4127 4244 4701 4706 4711 4702)
 #	define uint unsigned int
 #	define uchar unsigned char
 #	define execlp _execlp
 #	define strncasecmp strnicmp
 #	define strcasecmp stricmp
-//#	undef WINGDIAPI
-//#	define WINGDIAPI __declspec( dllexport )
-//#	undef APIENTRY
-//#	define APIENTRY __stdcall
-//#	undef CALLBACK
-//#	define CALLBACK __stdcall
-//#	undef XMLPARSEAPI
-//#	define XMLPARSEAPI __declspec( dllexport )
 #endif // WIN32
 
 #define M_PI (3.1415926535897932384626433832795)
-
-//#define DEBUG_OUT
 
 #endif /* __OSSPEC__H__ */ 
