b45634ba5c4bd29c7d47656f87357ff49d0bb1bd
pouillot <pouillot@30fe4595-0a0c-4342-8851-515496e4dcbd>
2010-09-07 21:34:19 +0000

    Fixed #59 : add uninstall target to the build system + generate clobber.sh/bat script for full in-source build files cleanup + moved files to the cmake folder
    git-svn-id: https://svn.code.sf.net/p/speed-dreams/code/trunk@2727 30fe4595-0a0c-4342-8851-515496e4dcbd
    
    Former-commit-id: e90d02cad90a1ea3631517e1258951a3b78c6d4c
    Former-commit-id: dd41213d98d09050ff7c15ae94e618fa56484ab6

---
+++ cmake/version.h.cmake
@@ -0,0 +1,13 @@
+#ifndef MK_VERSION_H
+#define MK_VERSION_H
+
+#ifndef VERSION
+#define VERSION "${VERSION}"
+#endif //VERSION
+
+#ifndef VERSION_LONG
+#define VERSION_LONG "${VERSION_LONG}"
+#endif //VERSION_LONG
+
+#endif //MK_VERSION_H
+
