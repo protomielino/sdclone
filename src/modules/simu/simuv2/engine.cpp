2f87e05da3f1014f438cab2b689afe9a6b78804c
kakukri <kakukri@30fe4595-0a0c-4342-8851-515496e4dcbd>
2010-10-29 17:54:22 +0000

    Temporary fix for #255
    git-svn-id: https://svn.code.sf.net/p/speed-dreams/code/trunk@3068 30fe4595-0a0c-4342-8851-515496e4dcbd
    
    Former-commit-id: 52e8be271599f5101d2dd23361e4ff6e40c8cb71
    Former-commit-id: 2b20a667dac4bfd426146064f4136b071d483e2c

--- src/modules/simu/simuv2/engine.cpp
+++ src/modules/simu/simuv2/engine.cpp
@@ -248,10 +248,12 @@ freerads += engine->Tq / engine->I * SimDeltaTime;
      engine->Tq = 0.0;
  } else if (engine->rads > engine->revsMax) {
      engine->rads = engine->revsMax;
-	 if (trans->curOverallRatio > 0)
+   if ( (trans->curOverallRatio > 0.01) || (trans->curOverallRatio < -0.01) )
        return engine->revsMax / trans->curOverallRatio;
-	 else
+   else {
+       GfLogError("Bug #225 triggered!\n");
        return engine->revsMax;
+   }
  }
 
  if ((trans->curOverallRatio!=0.0) && (I_response > 0)) {
