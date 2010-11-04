086c61690722af43a061dc55f0d1b0399fb936bd
kmetykog <kmetykog@30fe4595-0a0c-4342-8851-515496e4dcbd>
2010-11-04 22:21:51 +0000

    First step for single source USR. Re #51. WIP
    git-svn-id: https://svn.code.sf.net/p/speed-dreams/code/trunk@3092 30fe4595-0a0c-4342-8851-515496e4dcbd
    
    Former-commit-id: 6fdde044a70dff7ed612773614a37103990306fd
    Former-commit-id: 0d1bb6b1cdf06aa00469a38f1b9bab566c97fda7

---
+++ src/drivers/usr/mod.h
@@ -0,0 +1,80 @@
+/***************************************************************************
+
+    file                 : raceline.h
+    created              : Wed Mai 14 19:53:00 CET 2003
+    copyright            : (C) 2003-2004 by Bernhard Wymann
+    email                : berniw@bluewin.ch
+    version              : $Id$
+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef _MOD_H_
+#define _MOD_H_
+
+#define LMOD_DATA 200
+
+typedef struct {
+  double dval;
+  int ival;
+  int divstart;
+  int divend;
+} LRLModData;
+
+typedef struct {
+  LRLModData data[LMOD_DATA];
+  int used;
+} LRLMod;
+
+
+static void AddMod( LRLMod *mod, int divstart, int divend, double dval, int ival )
+{
+ if (!mod) return;
+
+ mod->data[mod->used].divstart = divstart;
+ mod->data[mod->used].divend = divend;
+ mod->data[mod->used].dval = dval;
+ mod->data[mod->used].ival = ival;
+ mod->used++;
+}
+
+
+static double GetModD( LRLMod *mod, int div )
+{
+ int i;
+
+ if (!mod)
+  return 0.0;
+
+ for (i=0; i<mod->used; i++)
+ {
+  if (div >= mod->data[i].divstart && div <= mod->data[i].divend)
+   return mod->data[i].dval;
+ }
+ return 0.0;
+}
+
+static int GetModI( LRLMod *mod, int div )
+{
+ int i;
+
+ if (!mod)
+  return 0;
+
+ for (i=0; i<mod->used; i++)
+ {
+  if (div >= mod->data[i].divstart && div <= mod->data[i].divend)
+   return mod->data[i].ival;
+ }
+ return 0;
+}
+
+#endif
