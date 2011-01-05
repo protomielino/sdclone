protomielino/sdclone

commit b08bc4d7982b46801edde0e43d6ddf5162241730

Re #241 (current race config preview in raceman menu) + enhanced tgfdata library about tracks + adapted Track Select menu to this (WIP) + fixed missing files in tgfdata
git-svn-id: https://svn.code.sf.net/p/speed-dreams/code/trunk@3192 30fe4595-0a0c-4342-8851-515496e4dcbd

Former-commit-id: 6fafb7b90bfb78ad40c079b0d21b9a9ae1c740f0
Former-commit-id: caba4004a50adbe72c214b2e78cea7903090f33f

src/libs/raceengineclient/raceconfigstate.cpp
@@ -1,0 +1,189 @@
+/***************************************************************************
+
+    file        : raceconfigstate.cpp
+    created     : Fri Jan  3 22:24:41 CET 2003
+    copyright   : (C) 2003 by Eric Espie                        
+    email       : eric.espie@torcs.org   
+    version     : $Id$
+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+/** @file   
+    		The automaton for race configuration
+    @author	<a href=mailto:eric.espie@torcs.org>Eric Espie</a>
+    @version	$Id$
+*/
+
+#include <cstdlib>
+#include <cstdio>
+
+#include <portability.h>
+#include <tgfclient.h>
+
+#include <raceman.h>
+
+#include <racescreens.h>
+
+#include "racesituation.h"
+//#include "racemain.h"
+//#include "raceinit.h"
+//#include "racestate.h"
+
+#include "raceenginemenus.h"
+
+
+// Data for the race configuration menus.
+static tRmTrackSelect  ts;
+static tRmDriverSelect ds;
+static tRmRaceParam    rp;
+
+
+static void
+reConfigBack(void)
+{
+	void* params = ReInfo->params;
+
+	/* Go back one step in the conf */
+	GfParmSetNum(params, RM_SECT_CONF, RM_ATTR_CUR_CONF, NULL, 
+				 GfParmGetNum(params, RM_SECT_CONF, RM_ATTR_CUR_CONF, NULL, 1) - 2);
+
+	ReConfigRunState();
+}
+
+
+/***************************************************************/
+/* Callback hooks used only to run the automaton on activation */
+static void	*configHookHandle = 0;
+
+static void
+configHookActivate(void * /* dummy */)
+{
+	ReConfigRunState();
+}
+
+static void *
+reConfigHookInit(void)
+{
+	if (configHookHandle)
+		return configHookHandle;
+
+	configHookHandle = GfuiHookCreate(0, configHookActivate);
+
+	return configHookHandle;
+}
+
+/***************************************************************/
+/* Config Back Hook */
+
+static void	*ConfigBackHookHandle = 0;
+
+static void
+ConfigBackHookActivate(void * /* dummy */)
+{
+	reConfigBack();
+}
+
+static void *
+reConfigBackHookInit(void)
+{
+	if (ConfigBackHookHandle)
+		return ConfigBackHookHandle;
+
+	ConfigBackHookHandle = GfuiHookCreate(0, ConfigBackHookActivate);
+
+	return ConfigBackHookHandle;
+}
+
+void
+ReConfigRunState(void)
+{
+	char	path[256];
+	int		i;
+	int		curConf;
+	const char	*conf;
+	int		numOpt;
+	const char	*opt;
+	void	*params = ReInfo->params;
+
+	curConf = (int)GfParmGetNum(params, RM_SECT_CONF, RM_ATTR_CUR_CONF, NULL, 1);
+	if (curConf > GfParmGetEltNb(params, RM_SECT_CONF)) {
+		GfLogInfo("%s configuration finished.\n", ReInfo->_reName);
+		GfParmWriteFile(NULL, ReInfo->params, ReInfo->_reName);
+		GfuiScreenActivate(ReGetRacemanMenuHandle()); /* Back to the race manager menu */
+		return;
+	}
+	
+	snprintf(path, sizeof(path), "%s/%d", RM_SECT_CONF, curConf);
+	conf = GfParmGetStr(params, path, RM_ATTR_TYPE, 0);
+	if (!conf) {
+		GfLogError("No %s here (%s) !\n", RM_ATTR_TYPE, path);
+		GfuiScreenActivate(ReGetRacemanMenuHandle()); /* Back to the race manager menu */
+		return;
+	}
+
+	GfLogInfo("%s configuration now in '%s' stage.\n", ReInfo->_reName, conf);
+	if (!strcmp(conf, RM_VAL_TRACKSEL)) {
+		/* Track Select Menu */
+		ts.nextScreen = reConfigHookInit();
+		if (curConf == 1) {
+			ts.prevScreen = ReGetRacemanMenuHandle();
+		} else {
+			ts.prevScreen = reConfigBackHookInit();
+		}
+		ts.param = ReInfo->params;
+		ts.trackItf = ReInfo->_reTrackItf;
+		RmTrackSelect(&ts);
+
+	} else if (!strcmp(conf, RM_VAL_DRVSEL)) {
+		/* Drivers select menu */
+		ds.nextScreen = reConfigHookInit();
+		if (curConf == 1) {
+			ds.prevScreen = ReGetRacemanMenuHandle();
+		} else {
+			ds.prevScreen = reConfigBackHookInit();
+		}
+		ds.param = ReInfo->params;
+		RmDriversSelect(&ds);
+
+	} else if (!strcmp(conf, RM_VAL_RACECONF)) {
+		/* Race Options menu */
+		rp.nextScreen = reConfigHookInit();
+		if (curConf == 1) {
+			rp.prevScreen = ReGetRacemanMenuHandle();
+		} else {
+			rp.prevScreen = reConfigBackHookInit();
+		}
+		rp.param = ReInfo->params;
+		rp.title = GfParmGetStr(params, path, RM_ATTR_RACE, "Race");
+		/* Select options to configure */
+		rp.confMask = 0;
+		snprintf(path, sizeof(path), "%s/%d/%s", RM_SECT_CONF, curConf, RM_SECT_OPTIONS);
+		numOpt = GfParmGetEltNb(params, path);
+		for (i = 1; i < numOpt + 1; i++) {
+			snprintf(path, sizeof(path), "%s/%d/%s/%d", RM_SECT_CONF, curConf, RM_SECT_OPTIONS, i);
+			opt = GfParmGetStr(params, path, RM_ATTR_TYPE, "");
+			if (!strcmp(opt, RM_VAL_CONFRACELEN)) {
+				/* Configure race length */
+				rp.confMask |= RM_CONF_RACE_LEN;
+			} else {
+				if (!strcmp(opt, RM_VAL_CONFDISPMODE)) {
+					/* Configure display mode */
+					rp.confMask |= RM_CONF_DISP_MODE;
+				}
+			}
+		}
+		RmRaceParamsMenu(&rp);
+	}
+
+	curConf++;
+	GfParmSetNum(params, RM_SECT_CONF, RM_ATTR_CUR_CONF, NULL, curConf);
+}

src/interfaces/track.h
@@ -52,9 +52,9 @@
 #define TRK_SECT_GRAPH	"Graphic"
 
 #define TRK_ATT_3DDESC	"3d description"
-#define TRK_ATT_3DDESC2 "3d description rain"
-#define TRK_ATT_3DDESC3 "3d description night"
-#define TRK_ATT_3DDESC4 "3d description rain+night"
+//#define TRK_ATT_3DDESC2 "3d description rain"
+//#define TRK_ATT_3DDESC3 "3d description night"
+//#define TRK_ATT_3DDESC4 "3d description rain+night"
 #define TRK_ATT_BKGRND	"background image"
 #define TRK_ATT_BGTYPE	"background type"
 #define TRK_ATT_BGCLR_R	"background color R"
@@ -542,7 +542,8 @@ typedef struct GraphicLightInfo
 
 typedef struct 
 {
-    const char		*background;
+    const char	*filename3d;	/**< Name of the track 3D description file (.ac/.acc) */
+    const char	*background;	/**< Name of the background image file (.png) */
     int			bgtype;
 #define TR_BACKGROUND_TYPE_0	0 /**< ??? Anyone who knows what's this */
 #define TR_BACKGROUND_TYPE_2	2 /**< ??? Anyone who knows what's this */
@@ -560,10 +561,11 @@ typedef struct
 /** Track structure
     @ingroup trackstruct
 */
-typedef struct
+typedef struct Track
 {
     const char	  *name;	/**< Name of the track */
-    const char	  *author;	/**< Author's name */
+    const char	  *descr;	/**< Description of the track */
+    const char	  *authors;	/**< Authors names */
     char	  *filename;	/**< Filename of the track description */
     void	  *params;	/**< Parameters handle */
     char	  *internalname; /**< Internal name of the track */
@@ -616,7 +618,7 @@ typedef struct
 
 
 
-typedef tTrack*(*tfTrackBuild)(char*);
+typedef tTrack*(*tfTrackBuild)(const char*);
 typedef tdble(*tfTrackHeightG)(tTrackSeg*, tdble, tdble);
 typedef tdble(*tfTrackHeightL)(tTrkLocPos*);
 typedef void(*tfTrackGlobal2Local)(tTrackSeg* /*seg*/, tdble /*X*/, tdble /*Y*/, tTrkLocPos* /*pos*/, int /*sides*/);
@@ -625,7 +627,8 @@ typedef void(*tfTrackSideNormal)(tTrackSeg*, tdble, tdble, int, t3Dd*);
 typedef void(*tfTrackSurfaceNormal)(tTrkLocPos *, t3Dd*);
 typedef void(*tfTrackShutdown)(void);
 
-typedef struct {
+typedef struct TrackItf
+{
     tfTrackBuild		trkBuild;		/* build track structure for simu */
     tfTrackBuild		trkBuildEx;		/* build with graphic extensions  */
     tfTrackHeightG		trkHeightG;

src/libs/raceengineclient/CMakeLists.txt
@@ -4,12 +4,14 @@ INCLUDE(../../../cmake/macros.cmake)
 
 SET(RACEENGINECLIENT_SOURCES racecareer.cpp raceupdate.cpp racenetwork.cpp racecars.cpp
 							 racemessage.cpp racegl.cpp raceinit.cpp racemain.cpp
-							 raceweather.cpp racemanmenu.cpp raceresults.cpp racesimusimu.cpp 
-							 racestate.cpp racesituation.cpp raceselectmenu.cpp networkingmenu.cpp
-							 raceengineclient.h 
+							 raceweather.cpp raceresults.cpp racesimusimu.cpp 
+							 racestate.cpp racesituation.cpp 
+                             racemanmenu.cpp raceselectmenu.cpp networkingmenu.cpp
+                             racenexteventmenu.cpp raceconfigstate.cpp
+							 raceengineclient.h raceenginemenus.h
 							 racecareer.h raceupdate.h racenetwork.h racecars.h
 							 racemessage.h racegl.h raceinit.h 
-							 raceweather.h racemanmenu.h raceresults.h racesimusimu.h 
+							 raceweather.h raceresults.h racesimusimu.h 
 							 racestate.h racesituation.h raceselectmenu.h networkingmenu.h)
 
 ADD_SD_DEFINITIONS()

src/libs/raceengineclient/networkingmenu.cpp
@@ -48,7 +48,7 @@ Should also allow choosing IP address, track, etc ...
 #include "racestate.h"
 #include "raceresults.h"
 
-#include "racemanmenu.h"
+#include "raceenginemenus.h"
 #include "networkingmenu.h"
 
 
@@ -611,7 +611,7 @@ ReNetworkHostMenu(void * /* dummy */)
 
 	g_ReadyCheckboxId = CreateCheckboxControl(racemanMenuHdle,mparam,"playerreadycheckbox",NULL,onHostPlayerReady);
 	g_HostSettingsButtonId = CreateButtonControl(racemanMenuHdle,mparam,"networkhostsettings",racemanMenuHdle,reNetworkHostSettingsMenu);
-	g_RaceSetupId = CreateButtonControl(racemanMenuHdle,mparam,"racesetup",racemanMenuHdle,ReConfigureMenu);
+	g_RaceSetupId = CreateButtonControl(racemanMenuHdle,mparam,"racesetup",racemanMenuHdle,ReConfigureRace);
 	g_CarSetupButtonId = CreateButtonControl(racemanMenuHdle,mparam,"car",racemanMenuHdle,reCarSettingsMenu);
 
 	CreateButtonControl(racemanMenuHdle,mparam,"start race",NULL,ServerPrepareStartNetworkRace);

src/libs/raceengineclient/raceenginemenus.h
@@ -1,6 +1,6 @@
 /***************************************************************************
 
-    file        : racemanmenu.h
+    file        : raceenginemenus.h
     created     : Fri Jan  3 22:25:02 CET 2003
     copyright   : (C) 2003 by Eric Espié                        
     email       : eric.espie@torcs.org   
@@ -23,18 +23,22 @@
     @version	$Id$
 */
 
-#ifndef _RACEMANMENU_H_
-#define _RACEMANMENU_H_
+#ifndef _RACEENGINEMENUS_H_
+#define _RACEENGINEMENUS_H_
 
 #include "raceengineclient.h"
 
 
 RACEENGINECLIENT_API int ReRacemanMenu(void);
-RACEENGINECLIENT_API int ReNewTrackMenu(void);
-RACEENGINECLIENT_API void ReConfigureMenu(void * /* dummy */);
-RACEENGINECLIENT_API void ReSetRacemanMenuHandle( void * handle);
+RACEENGINECLIENT_API int ReNextEventMenu(void);
+RACEENGINECLIENT_API void ReConfigureRace(void * /* dummy */);
+RACEENGINECLIENT_API void ReSetRacemanMenuHandle(void * handle);
 
-#endif /* _RACEMANMENU_H_ */ 
+extern void* ReGetRacemanMenuHandle();
+
+extern void ReConfigRunState(void);
+
+#endif /* _RACEENGINEMENU_H_ */ 
 
 
 

src/libs/raceengineclient/raceinit.cpp
@@ -797,12 +797,12 @@ reDumpTrack(const tTrack *track, int verbose)
 	char buf[128];
 	
 	snprintf(buf, sizeof(buf), "  by %s (%.0f m long, %.0f m wide) ...", 
-			 track->author, track->length, track->width);
+			 track->authors, track->length, track->width);
 	RmLoadingScreenSetText(buf);
 
 	GfLogInfo("++++++++++++ Track ++++++++++++\n");
 	GfLogInfo("Name     = %s\n", track->name);
-	GfLogInfo("Author   = %s\n", track->author);
+	GfLogInfo("Authors  = %s\n", track->authors);
 	GfLogInfo("Filename = %s\n", track->filename);
 	GfLogInfo("NSeg     = %d\n", track->nseg);
 	GfLogInfo("Version  = %d\n", track->version);

src/libs/raceengineclient/racemain.cpp
@@ -35,7 +35,7 @@
 #include "racegl.h"
 #include "raceresults.h"
 #include "racestate.h"
-#include "racemanmenu.h"
+#include "raceenginemenus.h"
 //#include "raceweatherupdate.h"
 
 #include "teammanager.h"
@@ -204,7 +204,7 @@ ReRaceEventInit(void)
 	ReEventInitResults();
 
 	if (GfParmGetEltNb(params, RM_SECT_TRACKS) > 1) {
-		ReNewTrackMenu();
+		ReNextEventMenu();
 		return RM_ASYNC | RM_NEXT_STEP;
 	}
 	return RM_SYNC | RM_NEXT_STEP;

commit 4b00c8649a7adac4cf37ba4b6c26c23b358eef9f

Re #241 (Race config preview in Raceman menu) Fixed regression in r3192 in Race Params (Cloud cover and Time of day no more customizable)
git-svn-id: https://svn.code.sf.net/p/speed-dreams/code/trunk@3243 30fe4595-0a0c-4342-8851-515496e4dcbd

Former-commit-id: c557e0db0a22eaa95bebcb6792dd903145f42e34
Former-commit-id: fb201bbe743cba30c2734a7bf8ec28a3b2d35710

src/libs/raceengineclient/raceconfigstate.cpp
@@ -132,6 +132,7 @@ ReConfigRunState(void)
 
 	GfLogInfo("%s configuration now in '%s' stage.\n", ReInfo->_reName, conf);
 	if (!strcmp(conf, RM_VAL_TRACKSEL)) {
+		
 		/* Track Select Menu */
 		ts.nextScreen = reConfigHookInit();
 		if (curConf == 1) {
@@ -144,6 +145,7 @@ ReConfigRunState(void)
 		RmTrackSelect(&ts);
 
 	} else if (!strcmp(conf, RM_VAL_DRVSEL)) {
+		
 		/* Drivers select menu */
 		ds.nextScreen = reConfigHookInit();
 		if (curConf == 1) {
@@ -155,6 +157,7 @@ ReConfigRunState(void)
 		RmDriversSelect(&ds);
 
 	} else if (!strcmp(conf, RM_VAL_RACECONF)) {
+		
 		/* Race Options menu */
 		rp.nextScreen = reConfigHookInit();
 		if (curConf == 1) {
@@ -164,6 +167,7 @@ ReConfigRunState(void)
 		}
 		rp.param = ReInfo->params;
 		rp.title = GfParmGetStr(params, path, RM_ATTR_RACE, "Race");
+		
 		/* Select options to configure */
 		rp.confMask = 0;
 		snprintf(path, sizeof(path), "%s/%d/%s", RM_SECT_CONF, curConf, RM_SECT_OPTIONS);
@@ -174,13 +178,41 @@ ReConfigRunState(void)
 			if (!strcmp(opt, RM_VAL_CONFRACELEN)) {
 				/* Configure race length */
 				rp.confMask |= RM_CONF_RACE_LEN;
-			} else {
-				if (!strcmp(opt, RM_VAL_CONFDISPMODE)) {
-					/* Configure display mode */
-					rp.confMask |= RM_CONF_DISP_MODE;
-				}
+			} else if (!strcmp(opt, RM_VAL_CONFDISPMODE)) {
+				/* Configure display mode */
+				rp.confMask |= RM_CONF_DISP_MODE;
+			} else if (!strcmp(opt, RM_VAL_CONFTIMEOFDAY)) {
+				/* Configure time of day */
+				rp.confMask |= RM_CONF_TIME_OF_DAY;
+			} else if (!strcmp(opt, RM_VAL_CONFCLOUDCOVER)) {
+				/* Configure cloud cover */
+				rp.confMask |= RM_CONF_CLOUD_COVER;
+			} else if (!strcmp(opt, RM_VAL_CONFRAINFALL)) {
+				/* Configure rain fall and dry/wet track */
+				rp.confMask |= RM_CONF_RAIN_FALL;
 			}
 		}
+
+		/* Check if really something we can configure (given the graphic options) */
+		if ((rp.confMask & (RM_CONF_TIME_OF_DAY | RM_CONF_CLOUD_COVER)) == rp.confMask) {
+ 	
+			snprintf(path, sizeof(path), "%s%s", GetLocalDir(), GR_PARAM_FILE);
+			void *grHandle = GfParmReadFile(path, GFPARM_RMODE_STD);
+			const bool bSkyDomeEnabled =
+				(int)GfParmGetNum(grHandle, GR_SCT_GRAPHIC, GR_ATT_SKYDOMEDISTANCE, NULL, 0) != 0;
+			GfParmReleaseHandle(grHandle);
+ 	
+			if (!bSkyDomeEnabled)
+			{
+				GfLogInfo("Skipping Race Params menu because Sky Dome is disabled"
+						  " and is needed for all the configurable options\n");
+				GfuiScreenActivate(ReGetRacemanMenuHandle()); /* Back to the race menu */
+				return;
+			}
+		}
+		GfLogTrace("Race configuration mask : 0x%02X\n", rp.confMask);
+ 	
+		/* All's right till now : enter the Race Params menu */
 		RmRaceParamsMenu(&rp);
 	}
 

src/libs/racescreens/raceparamsmenu.cpp
@@ -306,7 +306,9 @@ RmRaceParamsMenu(void *vrp)
 {
     char buf[256];
 
-    rp = (tRmRaceParam*)vrp;
+	GfLogTrace("Entering Race Params menu\n");
+
+	rp = (tRmRaceParam*)vrp;
 
 	// Get race features.
     rmrpFeatures = RmGetFeaturesList(rp->param);
@@ -398,7 +400,7 @@ RmRaceParamsMenu(void *vrp)
     }
 
     // Create and initialize Time of day combo box (2 arrow buttons and a variable label).
-    if (rp->confMask & RM_CONF_TIME_OF_DAY)
+	if (rp->confMask & RM_CONF_TIME_OF_DAY)
 	{
 		if (rmrpIsSkyDomeEnabled)
 		{
@@ -412,7 +414,7 @@ RmRaceParamsMenu(void *vrp)
 					break;
 				}
 		
-			// Create Display mode label.
+			// Create Time of day label.
 			CreateLabelControl(scrHandle,menuXMLDescHdle,"timeofdaylabel");
 
 			// Create and initialize Time of day combo-box-like control.
@@ -443,7 +445,7 @@ RmRaceParamsMenu(void *vrp)
 					break;
 				}
 			
-			// Create Display mode label.
+			// Create Cloud cover label.
 			CreateLabelControl(scrHandle,menuXMLDescHdle,"cloudslabel");
 
 			// Create and initialize Cloud cover combo-box-like control.
@@ -474,7 +476,7 @@ RmRaceParamsMenu(void *vrp)
 				break;
 			}
 			
-		// Create Display mode label.
+		// Create Rain label.
 		CreateLabelControl(scrHandle,menuXMLDescHdle,"rainlabel");
 
 		// Create and initialize Rain combo-box-like control.