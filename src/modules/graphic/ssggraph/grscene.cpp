4531af28ef52a28d354b227fb1b888c0ee629c0f
kmetykog <kmetykog@30fe4595-0a0c-4342-8851-515496e4dcbd>
2011-02-17 23:42:58 +0000

    Re #315. Pit wall segments are placed OK. Team logo displayed correctly at last. Wall starts at good position. Load wall textures only once.
    git-svn-id: https://svn.code.sf.net/p/speed-dreams/code/trunk@3371 30fe4595-0a0c-4342-8851-515496e4dcbd
    
    Former-commit-id: 84c96bc0ac0174324e79d01de363731b07899997
    Former-commit-id: f12c1242222493ac8103fb0df526a31bf41d23d3

--- src/modules/graphic/ssggraph/grscene.cpp
+++ src/modules/graphic/ssggraph/grscene.cpp
@@ -352,188 +352,239 @@ grCustomizePits(void)
 			break;
 
 		case TR_PIT_NO_BUILDING:
-			//Draw white, low wall (about 3ft high) with logos
+			{
+			//This mode draws a white, low wall (about 3ft high) with logos
+
+			//First we load the low wall's texture, as it is the same
+			// for all the drivers
+			snprintf(buf, sizeof(buf),"data/textures");
+			const std::string strWallFileName = "tr-bar-gr.png";
+			ssgState *stWall = grSsgLoadTexStateEx(strWallFileName.c_str(), buf, FALSE, FALSE, FALSE);
+			((ssgSimpleState*)stWall)->setShininess(100);
+
+			//Loop over all the available pits			
 			for(int i = 0; i < pits->nMaxPits; i++) {
-				tTrackOwnPit *actual_pit = &(pits->driversPits[i]);
-				ssgVertexArray *pit_vtx = new ssgVertexArray(4);
-				ssgTexCoordArray *pit_tex = new ssgTexCoordArray(4);
-				ssgColourArray *pit_clr = new ssgColourArray(1);
-				ssgNormalArray *pit_nrm = new ssgNormalArray(1);
-			
-				sgVec4 clr = {0, 0, 0, 1};
-				pit_clr->add(clr);
-		
+				tTrackOwnPit *act_pit = &(pits->driversPits[i]);
+
+				//Get this pit's center coords
+				tdble x0, y0;
+				t3Dd normalvector;
+				RtTrackLocal2Global(&(act_pit->pos), &x0, &y0, act_pit->pos.type);
+				RtTrackSideNormalG(act_pit->pos.seg, x0, y0, pits->side, &normalvector);
+				//This offset needed so the pit walls start at the correct plaze
+				x0 = x0 - pits->width / 2.0 * normalvector.x
+					- pits->len / 4.0 * normalvector.y;
+
+				//Load this pit's own logo
 				std::string strLogoFileName("logo"); // Default driver logo file name (pit door).
 
-				if (actual_pit->car[0]) {
-					
+				if (act_pit->car[0]) {
 					// If we have more than one car in the pit use the team pit logo of driver 0. 
 					snprintf(buf, sizeof(buf),
 							 "%sdrivers/%s/%d;%sdrivers/%s;drivers/%s/%d;drivers/%s;data/textures",
 							 GfLocalDir(),
-							 actual_pit->car[0]->_modName,
-							 actual_pit->car[0]->_driverIndex,
+							 act_pit->car[0]->_modName,
+							 act_pit->car[0]->_driverIndex,
 							 GfLocalDir(),
-							 actual_pit->car[0]->_modName,
-							 actual_pit->car[0]->_modName,
-							 actual_pit->car[0]->_driverIndex,
-							 actual_pit->car[0]->_modName);
+							 act_pit->car[0]->_modName,
+							 act_pit->car[0]->_modName,
+							 act_pit->car[0]->_driverIndex,
+							 act_pit->car[0]->_modName);
 
 					// If a custom skin was selected, and it can apply to the pit door,
 					// update the logo file name accordingly
-					if (strlen(actual_pit->car[0]->_skinName) != 0
-						&& actual_pit->car[0]->_skinTargets & RM_CAR_SKIN_TARGET_PIT_DOOR)
-					{
+					if (strlen(act_pit->car[0]->_skinName) != 0
+							&& act_pit->car[0]->_skinTargets & RM_CAR_SKIN_TARGET_PIT_DOOR) {
 						strLogoFileName += '-';
-						strLogoFileName += actual_pit->car[0]->_skinName;
+						strLogoFileName += act_pit->car[0]->_skinName;
 						GfLogTrace("Using skinned pit door logo %s\n", strLogoFileName.c_str());
 					}
-					
 				} else {
 					snprintf(buf, sizeof(buf), "data/textures");
-				}//if actual_pit->car[0]
-
-				// Load logo texture (.rgb first, for backwards compatibility, then .png)
-				const std::string strRGBLogoFileName = strLogoFileName + ".rgb";
-				ssgState *st = grSsgLoadTexStateEx(strRGBLogoFileName.c_str(), buf, FALSE, FALSE, FALSE);
-				if (!st)
-				{
-					const std::string strPNGLogoFileName = strLogoFileName + ".png";
-					st = grSsgLoadTexStateEx(strPNGLogoFileName.c_str(), buf, FALSE, FALSE);
-				}
-				((ssgSimpleState*)st)->setShininess(50);
-			
-				tdble x, y;
-				t3Dd normalvector;
-				RtTrackLocal2Global(&(actual_pit->pos), &x, &y, actual_pit->pos.type);
-				RtTrackSideNormalG(actual_pit->pos.seg, x, y, pits->side, &normalvector);
-				tdble x2 = x - pits->width / 2.0 * normalvector.x
-					+ pits->len / 2.0 * normalvector.y;
-				tdble y2 = y - pits->width / 2.0 * normalvector.y
-					- pits->len / 2.0 * normalvector.x;
-				tdble z2 = RtTrackHeightG(actual_pit->pos.seg, x2, y2);
-
-#if 0
-				//Let's draw the logo		
-				sgVec3 nrm;
-				nrm[0] = normalvector.x;
-				nrm[1] = normalvector.y;
-				nrm[2] = 0;
-				pit_nrm->add(nrm);
-
-				//First, bottom vertex
-				sgVec2 tex;
-				sgVec3 vtx;
-				tex[0] = 0.0;//-0.7;
-				tex[1] = 0.0;
-				vtx[0] = x2;
-				vtx[1] = y2;
-				vtx[2] = z2;
-				pit_tex->add(tex);
-				pit_vtx->add(vtx);
-
-				//First, top vertex
-				tex[0] = 0.0;//-0.7;
-				tex[1] = 0.33;
-				vtx[0] = x2;
-				vtx[1] = y2;
-				vtx[2] = z2 + 0.9;
-				pit_tex->add(tex);
-				pit_vtx->add(vtx);
-			
-				x2 = x - pits->width / 2.0 * normalvector.x
-					- pits->len / 1.0 * normalvector.y;
-				y2 = y - pits->width / 2.0 * normalvector.y
-					+ pits->len / 1.0 * normalvector.x;
-				z2 = RtTrackHeightG(pits->driversPits[i].pos.seg, x2, y2);
-
-				//Second, bottom vertex
-				tex[0] = 1.0;//1.3;
-				tex[1] = 0.0;
-				vtx[0] = x2;
-				vtx[1] = y2;
-				vtx[2] = z2;
-				pit_tex->add(tex);
-				pit_vtx->add(vtx);
-
-				//Second, top vertex
-				tex[0] = 1.0;//1.3;
-				tex[1] = 0.33;
-				vtx[0] = x2;
-				vtx[1] = y2;
-				vtx[2] = z2 + 0.9;
-				pit_tex->add(tex);
-				pit_vtx->add(vtx);
-
-				ssgVtxTable *pit = new ssgVtxTable(GL_TRIANGLE_STRIP, pit_vtx, pit_nrm, pit_tex, pit_clr);
-				pit->setState(st);
-				pit->setCullFace(0);
-				ThePits->addKid(pit);
-#endif
+				}//if act_pit->car[0]
 
 				//Let's draw the low wall
-				st = grSsgLoadTexStateEx("tr-bar-gr.png", buf, FALSE, FALSE);
-				((ssgSimpleState*)st)->setShininess(100);
-				x2 = x - pits->width / 2.0 * normalvector.x
-					+ pits->len * normalvector.y;
-				y2 = y - pits->width / 2.0 * normalvector.y
-					- pits->len * normalvector.x;
-				z2 = RtTrackHeightG(actual_pit->pos.seg, x2, y2);
+				//It is drawn in 3 parts.
+				//First some small wall, then the logo, then some more wall.
+				//Small wall bounds: (x1, y1) - (x2, y2)
+				//Logo part bounds:  (x2, y2) - (x3, y3)
+				//Small wall bounds: (x3, y3) - (x4, y4)
 
-				sgVec3 nrm2 = { normalvector.x, normalvector.y, 0.0 };
-				pit_nrm->add(nrm2);
+				tdble x1 = x0 - pits->width / 2.0 * normalvector.x
+					+ pits->len / 2.0 * normalvector.y;
+				tdble y1 = y0 - pits->width / 2.0 * normalvector.y
+					- pits->len / 2.0 * normalvector.x;
+				tdble z1 = RtTrackHeightG(act_pit->pos.seg, x1, y1);
 
+				tdble x2 = x0 - pits->width / 2.0 * normalvector.x
+					+ pits->len / 4.0 * normalvector.y;
+				tdble y2 = y0 - pits->width / 2.0 * normalvector.y
+					- pits->len / 4.0 * normalvector.x;
+				tdble z2 = RtTrackHeightG(act_pit->pos.seg, x2, y2);
+
+				tdble x3 = x0 - pits->width / 2.0 * normalvector.x
+					- pits->len / 4.0 * normalvector.y;
+				tdble y3 = y0 - pits->width / 2.0 * normalvector.y
+					+ pits->len / 4.0 * normalvector.x;
+				tdble z3 = RtTrackHeightG(act_pit->pos.seg, x3, y3);
+
+				tdble x4 = x0 - pits->width / 2.0 * normalvector.x
+					- pits->len / 2.0 * normalvector.y;
+				tdble y4 = y0 - pits->width / 2.0 * normalvector.y
+					+ pits->len / 2.0 * normalvector.x;
+				tdble z4 = RtTrackHeightG(act_pit->pos.seg, x4, y4);
+
+				ssgVertexArray *pit_vtx1 = new ssgVertexArray(4);
+				ssgTexCoordArray *pit_tex1 = new ssgTexCoordArray(4);
+				ssgColourArray *pit_clr1 = new ssgColourArray(1);
+				ssgNormalArray *pit_nrm1 = new ssgNormalArray(1);
+				sgVec4 clr1 = { 1, 1, 1, 1 };
+				pit_clr1->add(clr1);
+				sgVec3 nrm1 = { normalvector.x, normalvector.y, 0.0 };
+				pit_nrm1->add(nrm1);
+		
 				//First, bottom vertex
-				sgVec2 tex2 = { 0.0, 0.0 };
-				sgVec3 vtx2 = { x2, y2, z2 };
-				//~ tex2[0] = 0.0;
-				//~ tex2[1] = 0.0;
-				//~ vtx2[0] = x2;
-				//~ vtx2[1] = y2;
-				//~ vtx2[2] = z2;
-				pit_tex->add(tex2);
-				pit_vtx->add(vtx2);
+				{
+					sgVec2 tex = { 0.0, 0.0 };
+					sgVec3 vtx = { x1, y1, z1 };
+					pit_tex1->add(tex);
+					pit_vtx1->add(vtx);
+				}
 
 				//First, top vertex
-				//~ tex2 = { 0.0, 0.25 };
-				//~ vtx2 = { x2, y2, z2 + 0.9 };
-				tex2[0] = 0.0;
-				tex2[1] = 0.25;
-				vtx2[0] = x2;
-				vtx2[1] = y2;
-				vtx2[2] = z2 + 0.9;
-				pit_tex->add(tex2);
-				pit_vtx->add(vtx2);
+				{
+					sgVec2 tex = { 0.0, 0.25 };
+					sgVec3 vtx = { x1, y1, z1 + 0.9 };
+					pit_tex1->add(tex);
+					pit_vtx1->add(vtx);
+				}
 			
-				x2 = x - pits->width / 2.0 * normalvector.x
-					- pits->len * normalvector.y;
-				y2 = y - pits->width / 2.0 * normalvector.y
-					+ pits->len * normalvector.x;
-				z2 = RtTrackHeightG(pits->driversPits[i].pos.seg, x2, y2);
-
 				//Second, bottom vertex
-				tex2[0] = 1.0;
-				tex2[1] = 0.0;
-				vtx2[0] = x2;
-				vtx2[1] = y2;
-				vtx2[2] = z2;
-				pit_tex->add(tex2);
-				pit_vtx->add(vtx2);
+				{
+					sgVec2 tex = { 1.0, 0.0 };
+					sgVec3 vtx = { x2, y2, z2 };
+					pit_tex1->add(tex);
+					pit_vtx1->add(vtx);
+				}
+					
+				//Second, top vertex
+				{
+					sgVec2 tex = { 1.0, 0.25 };
+					sgVec3 vtx = { x2, y2, z2 + 0.9 };
+					pit_tex1->add(tex);
+					pit_vtx1->add(vtx);
+				}
+		
+				ssgVtxTable *pit = new ssgVtxTable(GL_TRIANGLE_STRIP, pit_vtx1, pit_nrm1, pit_tex1, pit_clr1);
+				pit->setState(stWall);
+				pit->setCullFace(0);
+				ThePits->addKid(pit);
+
+
+				//Let's draw the logo		
+				//Load logo texture (.rgb first, for backwards compatibility, then .png)
+				const std::string strRGBLogoFileName = strLogoFileName + ".rgb";
+				ssgState *stLogo = grSsgLoadTexStateEx(strRGBLogoFileName.c_str(), buf, FALSE, FALSE, FALSE);
+				if (!stLogo) {
+					const std::string strPNGLogoFileName = strLogoFileName + ".png";
+					stLogo = grSsgLoadTexStateEx(strPNGLogoFileName.c_str(), buf, FALSE, FALSE);
+				}
+				((ssgSimpleState*)stLogo)->setShininess(50);
+			
+				ssgVertexArray *pit_vtx2 = new ssgVertexArray(4);
+				ssgTexCoordArray *pit_tex2 = new ssgTexCoordArray(4);
+				ssgColourArray *pit_clr2 = new ssgColourArray(1);
+				ssgNormalArray *pit_nrm2 = new ssgNormalArray(1);
+				sgVec4 clr2 = { 1, 1, 1, 1 };
+				pit_clr2->add(clr2);
+				sgVec3 nrm2 = { normalvector.x, normalvector.y, 0.0 };
+				pit_nrm2->add(nrm2);
+				//First, bottom vertex
+				{
+					sgVec2 tex = { 0.0, 0.0 };
+					sgVec3 vtx = { x2, y2, z2 };
+					pit_tex2->add(tex);
+					pit_vtx2->add(vtx);
+				}
+
+				//First, top vertex
+				{
+					sgVec2 tex = { 0.0, 0.33 };
+					sgVec3 vtx = { x2, y2, z2 + 0.9 };
+					pit_tex2->add(tex);
+					pit_vtx2->add(vtx);
+				}
+			
+				//Second, bottom vertex
+				{
+					sgVec2 tex = { 1.0, 0.0 };
+					sgVec3 vtx = { x3, y3, z3 };
+					pit_tex2->add(tex);
+					pit_vtx2->add(vtx);
+				}
 
 				//Second, top vertex
-				tex2[0] = 1.0;
-				tex2[1] = 0.25;
-				vtx2[0] = x2;
-				vtx2[1] = y2;
-				vtx2[2] = z2 + 0.9;
-				pit_tex->add(tex2);
-				pit_vtx->add(vtx2);
-		
-				ssgVtxTable *pit2 = new ssgVtxTable(GL_TRIANGLE_STRIP, pit_vtx, pit_nrm, pit_tex, pit_clr);
-				pit2->setState(st);
-				pit2->setCullFace(false);
+				{
+					sgVec2 tex = { 1.0, 0.33 };
+					sgVec3 vtx = { x3, y3, z3 + 0.9 };
+					pit_tex2->add(tex);
+					pit_vtx2->add(vtx);
+				}
+
+				ssgVtxTable *pit2 = new ssgVtxTable(GL_TRIANGLE_STRIP, pit_vtx2, pit_nrm2, pit_tex2, pit_clr2);
+				pit2->setState(stLogo);
+				pit2->setCullFace(0);
 				ThePits->addKid(pit2);
+
+
+				//Draw 2nd wall
+				ssgVertexArray *pit_vtx3 = new ssgVertexArray(4);
+				ssgTexCoordArray *pit_tex3 = new ssgTexCoordArray(4);
+				ssgColourArray *pit_clr3 = new ssgColourArray(1);
+				ssgNormalArray *pit_nrm3 = new ssgNormalArray(1);
+				sgVec4 clr3 = { 1, 1, 1, 1 };
+				pit_clr3->add(clr3);
+				sgVec3 nrm3 = { normalvector.x, normalvector.y, 0.0 };
+				pit_nrm3->add(nrm3);
+				//First, bottom vertex
+				{
+					sgVec2 tex = { 0.0, 0.0 };
+					sgVec3 vtx = { x3, y3, z3 };
+					pit_tex3->add(tex);
+					pit_vtx3->add(vtx);
+				}
+
+				//First, top vertex
+				{
+					sgVec2 tex = { 0.0, 0.25 };
+					sgVec3 vtx = { x3, y3, z3 + 0.9 };
+					pit_tex3->add(tex);
+					pit_vtx3->add(vtx);
+				}
+			
+				//Second, bottom vertex
+				{
+					sgVec2 tex = { 1.0, 0.0 };
+					sgVec3 vtx = { x4, y4, z4 };
+					pit_tex3->add(tex);
+					pit_vtx3->add(vtx);
+				}
+
+				//Second, top vertex
+				{
+					sgVec2 tex = { 1.0, 0.25 };
+					sgVec3 vtx = { x4, y4, z4 + 0.9 };
+					pit_tex3->add(tex);
+					pit_vtx3->add(vtx);
+				}
+		
+				ssgVtxTable *pit3 = new ssgVtxTable(GL_TRIANGLE_STRIP, pit_vtx3, pit_nrm3, pit_tex3, pit_clr3);
+				pit3->setState(stWall);
+				pit3->setCullFace(0);
+				ThePits->addKid(pit3);
+
 			}//for i
+			}
 			break;
 			
 		case TR_PIT_ON_SEPARATE_PATH:
