protomielino/sdclone

commit 7770116d7f369891d8543f72c43d22705abef37e

Re #374 (migrated the ssggraph module to the new module scheme) and re #378 (removed the direct dependency of the race engine to the graphics engine)
git-svn-id: https://svn.code.sf.net/p/speed-dreams/code/trunk@3471 30fe4595-0a0c-4342-8851-515496e4dcbd

Former-commit-id: c34ed849a4756eddea78f5f54018f4d2f70a7cb5
Former-commit-id: d6c4224637985f264cba5c20dd53ad5477bb52dc

src/libs/raceengine/raceinit.cpp
@@ -157,9 +157,9 @@ void ReShutdown(void)
 	/* Free previous situation */
 	ReInfo->_reTrackItf.trkShutdown();
 
+	// Unload modules.
     GfModUnloadList(&reEventModList);
-
-    memset( &ReInfo->_reGraphicItf, 0, sizeof(tGraphicItf) );
+	RaceEngine::self().userInterface().shutdownGraphics();
 
     if (ReInfo->results) 
     {
@@ -866,43 +866,35 @@ ReInitCars(void)
 
 /**
  * This function initializes the graphics.
- * This function must be called after the cars are loaded and the track is loaded.
+ * It must be called after the cars are loaded and the track is loaded.
  * The track will be unloaded if the event ends. The graphics module is kept open
  * if more than one race is driven.
  */
 void ReInitGraphics()
 {
-  char const *dllname;
-  char key[256];
+	// Initialize the graphics engine.
+	if (RaceEngine::self().userInterface().initializeGraphics())
+	{
+		// Initialize the track graphics.
+		RaceEngine::self().userInterface().loadTrackGraphics(ReInfo->track);
 
-  /* Check if the module is already loaded */
-  if (ReInfo->_reGraphicItf.refresh)
-    return; /* Module is already loaded: don't load again */
-    
-  /* Load the graphic module */
-  GfLogInfo("Loading Graphic Engine...\n");
-  dllname = GfParmGetStr(ReInfo->_reParam, "Modules", "graphic", "");
-  snprintf(key, sizeof(key), "%smodules/graphic/%s.%s", GfLibDir(), dllname, DLLEXT);
-  if (GfModLoad(0, key, &reEventModList))
-	  return;
-  reEventModList->modInfo->fctInit(reEventModList->modInfo->index, &ReInfo->_reGraphicItf);
-
-  ReInfo->_reGraphicItf.inittrack(ReInfo->track);
+		// Initialize the graphics view.
+		RaceEngine::self().userInterface().setupGraphicsView();
+	}
 }
 
 void
 ReRaceCleanup(void)
 {
   ReInfo->_reGameScreen = RaceEngine::self().userInterface().createRaceEventLoopHook();
+
   ReInfo->_reSimItf.shutdown();
+
   if (ReInfo->_displayMode == RM_DISP_MODE_NORMAL) 
-  {
-    if (ReInfo->_reGraphicItf.shutdowncars)
-      ReInfo->_reGraphicItf.shutdowncars();
-    else
-      GfLogError("WARNING: normal race display but graphical module not loaded !\n" );
-  }
+	  RaceEngine::self().userInterface().unloadCarsGraphics();
+
   ReStoreRaceResults(ReInfo->_reRaceName);
+
   ReRaceCleanDrivers();
 }
 

src/libs/raceengine/racemain.cpp
@@ -27,8 +27,6 @@
 #include <robot.h>
 #include <network.h>
 
-#include <tgfclient.h> // TODO: Remove when GfScrGetSize no more needed.
-
 #include "raceengine.h"
 
 #include "raceutil.h" // RmGetFeaturesList
@@ -82,13 +80,14 @@ void ReRaceAbort()
 	ReShutdownUpdaters();
 
 	ReInfo->_reSimItf.shutdown();
-	if (ReInfo->_displayMode == RM_DISP_MODE_NORMAL) {
-		if (ReInfo->_reGraphicItf.shutdowncars)
-			ReInfo->_reGraphicItf.shutdowncars();
-	}
 
-	if (ReInfo->_reGraphicItf.shutdowntrack)
-		ReInfo->_reGraphicItf.shutdowntrack();
+	// TODO: Move these 2 unloadXXGrahics calls to the user interface module ?
+	if (ReInfo->_displayMode == RM_DISP_MODE_NORMAL)
+		RaceEngine::self().userInterface().unloadCarsGraphics();
+
+	// TODO: only if (ReInfo->_displayMode == RM_DISP_MODE_NORMAL) ?
+	RaceEngine::self().userInterface().unloadTrackGraphics();
+	
 	ReRaceCleanDrivers();
 
 	if (GetNetwork())
@@ -255,7 +254,6 @@ int
 ReRaceRealStart(void)
 {
 	int i, j;
-	int sw, sh, vw, vh;
 	tRobotItf *robot;
 	tReCarInfo *carInfo;
 	const char *dllname;
@@ -294,7 +292,6 @@ ReRaceRealStart(void)
 	/* Blind mode or not */
 	ReInfo->_displayMode = RM_DISP_MODE_NORMAL;
 	ReInfo->_reGameScreen = RaceEngine::self().userInterface().createRaceScreen();
-	//foundHuman = 0;
 
 	//Check if there is a human in the current race
 	for (i = 0; i < s->_ncars; i++) {
@@ -345,11 +342,12 @@ ReRaceRealStart(void)
 	carInfo = ReInfo->_reCarInfo;
 	RtTeamManagerStart();
 
-	/* Initialize graphical module */
+	// Initialize graphics engine
 	if (ReInfo->_displayMode == RM_DISP_MODE_NORMAL
 		|| ReInfo->_displayMode == RM_DISP_MODE_CAPTURE)
 		ReInitGraphics();
 
+	// Initialize physics engine
 	ReInfo->_reSimItf.update(s, RCM_MAX_DT_SIMU, -1);
 	for (i = 0; i < s->_ncars; i++) {
 		carInfo[i].prevTrkPos = s->cars[i]->_trkPos;
@@ -386,10 +384,6 @@ ReRaceRealStart(void)
 	ReInfo->s->deltaTime = RCM_MAX_DT_SIMU;
 	ReInfo->s->_raceState = RM_RACE_STARTING;
 
-	GfScrGetSize(&sw, &sh, &vw, &vh); // Last tgfclient dependency.
-	if (ReInfo->_reGraphicItf.initview)
-		ReInfo->_reGraphicItf.initview((sw-vw)/2, (sh-vh)/2, vw, vh, GR_VIEW_STD, ReInfo->_reGameScreen);
-
 	ReInfo->_reInPitMenuCar = 0;
 	ReInfo->_reMessage = 0;
 	ReInfo->_reMessageEnd = 0.0;
@@ -397,10 +391,11 @@ ReRaceRealStart(void)
 	ReInfo->_reBigMessageEnd = 0.0;
 	
 	ReInitUpdaters();
-	
+
+	// Initalize cars graphics.
 	if (ReInfo->_displayMode == RM_DISP_MODE_NORMAL) {
 		RaceEngine::self().userInterface().addLoadingMessage("Loading cars ...");
-		ReInitCarGraphics();
+		RaceEngine::self().userInterface().loadCarsGraphics(ReInfo->s);
 	}
 
 	if (GetNetwork())

src/interfaces/CMakeLists.txt
@@ -2,6 +2,6 @@ INCLUDE(../../cmake/macros.cmake)
 
 SET(INTERFACES_HEADERS car.h graphic.h playerpref.h raceman.h
 	               replay.h robot.h simu.h telemetry.h track.h
-                       iraceengine.h iuserinterface.h)
+                       iraceengine.h iuserinterface.h igraphicsengine.h)
 
 SD_INSTALL_FILES(INCLUDE FILES ${INTERFACES_HEADERS})

src/interfaces/igraphicsengine.h
@@ -1,0 +1,45 @@
+/***************************************************************************
+                 igraphicsengine.h -- Interface for graphics engines
+
+    created              : Mon Mar 28 19:48:14 CEST 2011
+    copyright            : (C) 2011 by Jean-Philippe Meuret                         
+    web                  : http://www.speed-dreams.org
+    version              : $Id$
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+/** @file   
+    	Interface for graphics engines
+    @version	$Id$
+*/
+
+#ifndef __IGRAPHICSENGINE__H__
+#define __IGRAPHICSENGINE__H__
+
+#include <graphic.h>
+
+
+class IGraphicsEngine
+{
+public:
+
+	virtual bool loadTrack(struct Track* pTrack) = 0;
+	virtual bool loadCars(struct Situation *pSituation) = 0;
+	virtual bool setupView(int x, int y, int width, int height, void* pMenuScreen) = 0;
+	virtual void updateView(struct Situation *pSituation) = 0;
+	virtual void shutdownView() = 0;
+	virtual void unloadCars() = 0;
+	virtual void unloadTrack() = 0;
+
+	//virtual void bendCar(int index, sgVec3 poc, sgVec3 force, int count = 0) = 0;
+};
+
+#endif // __IGRAPHICSENGINE__H__

src/interfaces/iuserinterface.h
@@ -76,6 +76,16 @@ public:
 
 	virtual void activateStandingsMenu(void *prevHdle, struct RmInfo *info, int start = 0) = 0;
 
+	// Graphics engine control.
+	virtual bool initializeGraphics() = 0;
+	virtual bool loadTrackGraphics(struct Track* pTrack) = 0;
+	virtual bool loadCarsGraphics(struct Situation *pSituation) = 0;
+	virtual bool setupGraphicsView() = 0;
+	virtual void updateGraphicsView(struct Situation *pSituation) = 0;
+	virtual void unloadCarsGraphics() = 0;
+	virtual void unloadTrackGraphics() = 0;
+	virtual void shutdownGraphics() = 0;
+
 	virtual void setRaceEngine(IRaceEngine& raceEngine) = 0;
 };
 

src/interfaces/raceman.h
@@ -30,7 +30,8 @@
 #include <tgf.h>
 #include <car.h>
 #include <track.h>
-#include <graphic.h>
+// TODO: Remove (old graphics setup)
+//#include <graphic.h>
 #include <simu.h>
 
 #define RCM_IDENT 0
@@ -105,7 +106,8 @@ typedef struct Situation {
 typedef struct 
 {
     tTrackItf	trackItf;
-    tGraphicItf	graphicItf;
+	// TODO: Remove (old graphics setup)
+    //tGraphicItf	graphicItf;
     tSimItf	simItf;
 } tRaceModIft;
 
@@ -171,7 +173,8 @@ typedef struct
 #define _reState	raceEngineInfo.state
 #define _reParam	raceEngineInfo.param
 #define _reTrackItf	raceEngineInfo.itf.trackItf
-#define _reGraphicItf	raceEngineInfo.itf.graphicItf
+// TODO: Remove (old graphics setup)
+//#define _reGraphicItf	raceEngineInfo.itf.graphicItf
 #define _reSimItf	raceEngineInfo.itf.simItf
 #define _reGameScreen	raceEngineInfo.gameScreen
 #define _reMenuScreen	raceEngineInfo.menuScreen

src/libs/raceengine/racenetwork.cpp
@@ -224,7 +224,7 @@ ReNetworkWaitReady()
 		ReInfo->_refreshDisplay = 1;
 		RaceEngine::self().userInterface().update();
 
-		ReInfo->_reGraphicItf.refresh(ReInfo->s);
+		RaceEngine::self().userInterface().updateGraphicsView(ReInfo->s);
 	}
 
 	return mode;

src/libs/raceengine/racesituation.cpp
@@ -89,7 +89,8 @@ tRmInfo* ReSituationAllocInit(const tRmInfo* pSource)
 	// Assign level 2 constants in raceEngineInfo field.
 	pTarget->_reParam = pSource->_reParam; // Not used / written by updater.
 	pTarget->_reTrackItf = pSource->_reTrackItf; // Not used / written by updater.
-	pTarget->_reGraphicItf = pSource->_reGraphicItf; // Not used / written by updater.
+	// TODO : Remove (old graphics setup)
+	//	pTarget->_reGraphicItf = pSource->_reGraphicItf; // Not used / written by updater.
 	pTarget->_reSimItf = pSource->_reSimItf; // Not used / written by updater.
 	pTarget->_reGameScreen = pSource->_reGameScreen; // Nor changed nor shared during the race.
 	pTarget->_reMenuScreen = pSource->_reMenuScreen; // Nor changed nor shared during the race.

commit 2a36dfe2a17bfca90bd2fbbda962721cfc426f92

Fixed #380 Broken track map after r3471 : restored the call to ssggraph::setupView to its right place in racemain::ReRaceRealStart
git-svn-id: https://svn.code.sf.net/p/speed-dreams/code/trunk@3486 30fe4595-0a0c-4342-8851-515496e4dcbd

Former-commit-id: 7f0ddd44737289c6e92a04ded4f736d6ca93083d
Former-commit-id: e38bb6470528bd062f5fb6ae92493750091a699b

src/libs/raceengine/raceinit.cpp
@@ -894,9 +894,6 @@ void ReInitGraphics()
 	{
 		// Initialize the track graphics.
 		RaceEngine::self().userInterface().loadTrackGraphics(ReInfo->track);
-
-		// Initialize the graphics view.
-		RaceEngine::self().userInterface().setupGraphicsView();
 	}
 }
 

src/libs/raceengine/racemain.cpp
@@ -289,7 +289,7 @@ ReRaceRealStart(void)
 		return RM_ERROR;
 	}
 
-	/* Blind mode or not */
+	// Blind mode or not
 	ReInfo->_displayMode = RM_DISP_MODE_NORMAL;
 	ReInfo->_reGameScreen = RaceEngine::self().userInterface().createRaceScreen();
 
@@ -342,24 +342,24 @@ ReRaceRealStart(void)
 	carInfo = ReInfo->_reCarInfo;
 	RtTeamManagerStart();
 
-	// Initialize graphics engine
+	// Initialize the graphics engine
 	if (ReInfo->_displayMode == RM_DISP_MODE_NORMAL
 		|| ReInfo->_displayMode == RM_DISP_MODE_CAPTURE)
 		ReInitGraphics();
 
-	// Initialize physics engine
+	// Initialize the physics engine
 	ReInfo->_reSimItf.update(s, RCM_MAX_DT_SIMU, -1);
 	for (i = 0; i < s->_ncars; i++) {
 		carInfo[i].prevTrkPos = s->cars[i]->_trkPos;
 	}
 
-	//All cars start with max brakes on
+	// All cars start with max brakes on
 	RaceEngine::self().userInterface().addLoadingMessage("Running Prestart ...");
 	for (i = 0; i < s->_ncars; i++) {
 		memset(&(s->cars[i]->ctrl), 0, sizeof(tCarCtrl));
 		s->cars[i]->ctrl.brakeCmd = 1.0;
 	}
-	for (j = 0; j < ((int)(1.0 / RCM_MAX_DT_SIMU)); j++) {
+	for (j = 0; j < (int)(1.0 / RCM_MAX_DT_SIMU); j++) {
 		ReInfo->_reSimItf.update(s, RCM_MAX_DT_SIMU, -1);
 	}
 
@@ -380,9 +380,12 @@ ReRaceRealStart(void)
 	if (GetNetwork())
 		ReInfo->s->currentTime = GfTimeClock() - GetNetwork()->GetRaceStartTime();
 	else
-		ReInfo->s->currentTime = -2.0;	//we start 2 seconds before the start
+		ReInfo->s->currentTime = -2.0;	//we start 2 seconds before the real race start
 	ReInfo->s->deltaTime = RCM_MAX_DT_SIMU;
 	ReInfo->s->_raceState = RM_RACE_STARTING;
+
+	// Initialize the graphics view.
+	RaceEngine::self().userInterface().setupGraphicsView();
 
 	ReInfo->_reInPitMenuCar = 0;
 	ReInfo->_reMessage = 0;