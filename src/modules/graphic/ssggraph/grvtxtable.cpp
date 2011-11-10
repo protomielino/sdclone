acbfabbb438df801b1daea50c22219ed7e5f4089
pouillot <pouillot@30fe4595-0a0c-4342-8851-515496e4dcbd>
2011-11-10 18:29:06 +0000

    Reduced the speed of the projection of the track env. texture on the cars, by a factor 2 (from Eckhards tests) (forgotten to also change 1 line, probably never used BTW)
    git-svn-id: https://svn.code.sf.net/p/speed-dreams/code/trunk@4130 30fe4595-0a0c-4342-8851-515496e4dcbd
    
    Former-commit-id: 9fbd9c860dffc458153f507b3aec2ee32b3d8974
    Former-commit-id: 6fc24da75cab836254877dfbcb32d374abf0c866

--- src/modules/graphic/ssggraph/grvtxtable.cpp
+++ src/modules/graphic/ssggraph/grvtxtable.cpp
@@ -496,7 +496,7 @@ void cgrVtxTableCarPart::draw_geometry ()
 		glEnable(GL_TEXTURE_2D);
 		glMatrixMode(GL_TEXTURE);
 		glLoadIdentity();
-		ttx = grCarInfo[_carIndex].distFromStart/50;
+		ttx = grCarInfo[_carIndex].distFromStart/100;
 		sgMakeTransMat4(mat, ttx, tty, ttz);
 		glMultMatrixf((float *)mat);
 		glMatrixMode(GL_MODELVIEW);
