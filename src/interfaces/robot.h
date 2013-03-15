5afb7c0a4d0c1024c09480d3d9157a219aba5015
kmetykog <kmetykog@30fe4595-0a0c-4342-8851-515496e4dcbd>
2013-03-15 00:22:00 +0000

    Re #572: add 3-letter code to robot attributes
    git-svn-id: https://svn.code.sf.net/p/speed-dreams/code/trunk@5319 30fe4595-0a0c-4342-8851-515496e4dcbd
    
    Former-commit-id: 91fb83b7f2506de23b87e6eb3dc3f320c2860c22
    Former-commit-id: f8a7af0e8164e9ee821333893638856b8ac9337e

--- src/interfaces/robot.h
+++ src/interfaces/robot.h
@@ -88,6 +88,7 @@ typedef struct RobotItf {
 
 #define ROB_ATTR_NAME		"name"
 #define ROB_ATTR_SNAME		"short name"
+#define ROB_ATTR_CODE		"code name" //3-letter abbreviated name
 #define ROB_ATTR_TEAM		"team"
 #define ROB_ATTR_DESC		"desc"
 #define ROB_ATTR_AUTHOR		"author"
