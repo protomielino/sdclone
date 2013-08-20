75f9d8b3a5a6abd43f7c7d0622472ff336be88fc
torcs-ng <torcs-ng@30fe4595-0a0c-4342-8851-515496e4dcbd>
2013-08-20 21:50:38 +0000

    update check_skins.sh for good path drivers
    git-svn-id: https://svn.code.sf.net/p/speed-dreams/code/trunk@5689 30fe4595-0a0c-4342-8851-515496e4dcbd
    
    Former-commit-id: b953699a94a28f800a832ab01da7e637a787e7fa
    Former-commit-id: d1be2ec2d2d7db2ac5b3c4d7efb8fe72340813f9

--- src/tools/scripts/check_skins.sh
+++ src/tools/scripts/check_skins.sh
@@ -9,7 +9,7 @@ versioning="-s"
 # Process all files regardless of versions
 #versioning="-a"
 
-drivers="`pwd`/../../drivers/"
+drivers="`pwd`/../../../data/drivers"
 cars="`pwd`/../../../data/cars/models/"
 
 # Enable the screen shots
@@ -23,21 +23,22 @@ run="-r /usr/local/games/speed-dreams-2"
 python check_car_skins.py -c $cars $versioning $config $run $proc
 
 # Usr
-python check_robot_skins.py -d $drivers/usr/usr_36GP -c $cars $versioning $config $run $proc usr_36GP.xml
-python check_robot_skins.py -d $drivers/usr/usr_ls1  -c $cars $versioning $config $run $proc usr_ls1.xml
-python check_robot_skins.py -d $drivers/usr/usr_ls2  -c $cars $versioning $config $run $proc usr_ls2.xml
-python check_robot_skins.py -d $drivers/usr/usr_sc   -c $cars $versioning $config $run $proc usr_sc.xml
-python check_robot_skins.py -d $drivers/usr/usr_trb1 -c $cars $versioning $config $run $proc usr_trb1.xml
-python check_robot_skins.py -d $drivers/usr/usr_rs   -c $cars $versioning $config $run $proc usr_rs.xml
+python check_robot_skins.py -d $drivers/usr_36GP -c $cars $versioning $config $run $proc usr_36GP.xml
+python check_robot_skins.py -d $drivers/usr_ls1  -c $cars $versioning $config $run $proc usr_ls1.xml
+python check_robot_skins.py -d $drivers/usr_ls2  -c $cars $versioning $config $run $proc usr_ls2.xml
+python check_robot_skins.py -d $drivers/usr_mpa1 -c $cars $versioning $config $run $proc usr_mpa1.xml
+python check_robot_skins.py -d $drivers/usr_sc   -c $cars $versioning $config $run $proc usr_sc.xml
+python check_robot_skins.py -d $drivers/usr_trb1 -c $cars $versioning $config $run $proc usr_trb1.xml
+python check_robot_skins.py -d $drivers/usr_rs   -c $cars $versioning $config $run $proc usr_rs.xml
 
 # Simplix
-python check_robot_skins.py -d $drivers/simplix/simplix_36GP -c $cars $versioning $config $run $proc simplix_36GP.xml
-python check_robot_skins.py -d $drivers/simplix/simplix_ls1  -c $cars $versioning $config $run $proc simplix_ls1.xml
-python check_robot_skins.py -d $drivers/simplix/simplix_ls2  -c $cars $versioning $config $run $proc simplix_ls2.xml
-python check_robot_skins.py -d $drivers/simplix/simplix_mp5  -c $cars $versioning $config $run $proc simplix_mp5.xml
-python check_robot_skins.py -d $drivers/simplix/simplix_sc   -c $cars $versioning $config $run $proc simplix_sc.xml
-python check_robot_skins.py -d $drivers/simplix/simplix_trb1 -c $cars $versioning $config $run $proc simplix_trb1.xml
-python check_robot_skins.py -d $drivers/simplix/simplix_mpa1 -c $cars $versioning $config $run $proc simplix_mpa1.xml
+python check_robot_skins.py -d $drivers/simplix_36GP -c $cars $versioning $config $run $proc simplix_36GP.xml
+python check_robot_skins.py -d $drivers/simplix_ls1  -c $cars $versioning $config $run $proc simplix_ls1.xml
+python check_robot_skins.py -d $drivers/simplix_ls2  -c $cars $versioning $config $run $proc simplix_ls2.xml
+python check_robot_skins.py -d $drivers/simplix_mp5  -c $cars $versioning $config $run $proc simplix_mp5.xml
+python check_robot_skins.py -d $drivers/simplix_sc   -c $cars $versioning $config $run $proc simplix_sc.xml
+python check_robot_skins.py -d $drivers/simplix_trb1 -c $cars $versioning $config $run $proc simplix_trb1.xml
+python check_robot_skins.py -d $drivers/simplix_mpa1 -c $cars $versioning $config $run $proc simplix_mpa1.xml
 
 # Kilo2008
 python check_robot_skins.py -d $drivers/kilo2008/ -c $cars $versioning $config $run $proc kilo2008.xml
