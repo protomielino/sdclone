a3045c2eb5f03f8096783b4dd87427d2d38d38c1
torcs-ng <torcs-ng@30fe4595-0a0c-4342-8851-515496e4dcbd>
2015-04-01 21:43:04 +0000

    - push OsgOptions files
    git-svn-id: https://svn.code.sf.net/p/speed-dreams/code/trunk@5939 30fe4595-0a0c-4342-8851-515496e4dcbd
    
    Former-commit-id: bdd31937c428ea14a9a894e8dd7e2afd39abcf2c
    Former-commit-id: 55e85aadee0290851e2c6cd2fa056f24711702ab

---
+++ src/modules/graphic/osggraph/Utils/OsgOptions.h
@@ -0,0 +1,35 @@
+/***************************************************************************
+
+    file                     : OsgOptions.h
+    created                  : Thu Mar 31 00:00:41 CEST 2015
+    copyright                : (C) 2015 by Xavier Bertaux
+    email                    : bertauxx@yahoo.fr
+    version                  : $Id: OsgOptions.h 4693 2012-04-13 03:12:09Z torcs-ng $
+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include <stdlib.h>
+
+#ifndef _OSGOPTIONS_H_
+#define _OSGOPTIONS_H_
+
+class SDOptions
+{
+public:
+    SDOptions( void );
+    ~SDOptions( void );
+
+private:
+
+};
+
+#endif /* _OSGOPTIONS_H_ */
