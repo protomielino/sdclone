4196448e15b0ae1b3b36fc0b5f9ca554317e7f52
torcs-ng <torcs-ng@30fe4595-0a0c-4342-8851-515496e4dcbd>
2019-11-20 15:42:54 +0000

    - commit Ivan's patch  (limit the count simulation steph by frame)
    git-svn-id: https://svn.code.sf.net/p/speed-dreams/code/trunk@6775 30fe4595-0a0c-4342-8851-515496e4dcbd
    
    Former-commit-id: f7a2a8e9278a66394adb8fb7e52df208a67f9083
    Former-commit-id: e3e37969266081ff320316ee6559875f8e8b6d03

--- src/modules/racing/standardgame/racesituation.h
+++ src/modules/racing/standardgame/racesituation.h
@@ -2,7 +2,7 @@
 
     file        : racesituation.h
     copyright   : (C) 2010 by Jean-Philippe Meuret
-    web         : www.speed-dreams.org 
+    web         : www.speed-dreams.org
     version     : $Id$
 
  ***************************************************************************/
@@ -15,9 +15,9 @@
  *   (at your option) any later version.                                   *
  *                                                                         *
  ***************************************************************************/
- 
-/** @file    
-    		
+
+/** @file
+
     @author	    Jean-Philippe Meuret
     @version	$Id$
 */
@@ -34,150 +34,153 @@
 class ReSituation
 {
 public:
-	
-	//! Accessor to the singleton (not thread-safe at instanciation time).
-	static ReSituation& self();
 
-	//! Delete the singleton.
-	static void terminate();
-	
-	//! Destructor.
-	~ReSituation();
+    //! Accessor to the singleton (not thread-safe at instanciation time).
+    static ReSituation& self();
 
-	// TODO: Remove when all necessary safe accessors ready.
-	struct RmInfo* data();
+    //! Delete the singleton.
+    static void terminate();
 
-	//! Activate/Deactivate the thread-safe mode.
-	void setThreadSafe(bool bOn = true);
+    //! Destructor.
+    ~ReSituation();
 
-	//! Safe accessors.
-	void setDisplayMode(unsigned bfDispMode);
-	void accelerateTime(double fMultFactor);
-	void setRaceMessage(const std::string& strMsg, double fLifeTime = -1, bool bBig = false);
-	
-	//! Set pit command for the given car (back from a pit menu or so).
-	void setPitCommand(int nCarIndex, const tCarPitCmd *pPitCmd);
+    // TODO: Remove when all necessary safe accessors ready.
+    struct RmInfo* data();
 
-private:
+    //! Activate/Deactivate the thread-safe mode.
+    void setThreadSafe(bool bOn = true);
 
-	//! Protected constructor to avoid instanciation outside of self().
-	ReSituation();
+    //! Safe accessors.
+    void setDisplayMode(unsigned bfDispMode);
+    void accelerateTime(double fMultFactor);
+    void setRaceMessage(const std::string& strMsg, double fLifeTime = -1, bool bBig = false);
 
-	//! Lock the data when thread-safe mode activated (otherwise do nothing).
-	bool lock(const char* pszCallerName = 0);
-	
-	//! Unlock the data when thread-safe mode activated (otherwise do nothing).
-	bool unlock(const char* pszCallerName = 0);
+    //! Set pit command for the given car (back from a pit menu or so).
+    void setPitCommand(int nCarIndex, const tCarPitCmd *pPitCmd);
 
 private:
 
-	//! The singleton.
-	static ReSituation* _pSelf;
+    //! Protected constructor to avoid instanciation outside of self().
+    ReSituation();
 
-	//! The real data behind.
-	struct RmInfo *_pReInfo;
-	
-	//! The mutex for thread-safe access.
-	struct SDL_mutex* _pMutex;
+    //! Lock the data when thread-safe mode activated (otherwise do nothing).
+    bool lock(const char* pszCallerName = 0);
 
-	// ReSituationUpdater is a very good friend 8-D.
-	friend class ReSituationUpdater;
+    //! Unlock the data when thread-safe mode activated (otherwise do nothing).
+    bool unlock(const char* pszCallerName = 0);
+
+private:
+
+    //! The singleton.
+    static ReSituation* _pSelf;
+
+    //! The real data behind.
+    struct RmInfo *_pReInfo;
+
+    //! The mutex for thread-safe access.
+    struct SDL_mutex* _pMutex;
+
+    // ReSituationUpdater is a very good friend 8-D.
+    friend class ReSituationUpdater;
 };
 
 // The race situation updater class ==================================================
 class ReSituationUpdater
 {
 public:
-	
-	//! Constructor.
-	ReSituationUpdater();
 
-	//! Destructor.
-	~ReSituationUpdater();
+    //! Constructor.
+    ReSituationUpdater();
 
-	//! Set scheduling specs (see below _fSimuTick and _fOutputTick).
-	bool setSchedulingSpecs(double fSimuRate, double fOutputRate = 0);
+    //! Destructor.
+    ~ReSituationUpdater();
 
-	//! (Re)start (after a stop) the updater if it is not running.
-	void start();
-	
-	//! Stop (pause) the updater if it is running (return its exit status).
-	void stop();
-	
-	//! Terminate the updater (return its exit status ; wait for the thread to return).
-	int terminate();
-	
-	//! Get the situation for the previous step
-	struct RmInfo* getPreviousStep();
-	
-	//! Compute 1 situation update step.
-	void runOneStep(double deltaTimeIncrement);
+    //! Set scheduling specs (see below _fSimuTick and _fOutputTick).
+    bool setSchedulingSpecs(double fSimuRate, double fOutputRate = 0);
 
-	//! Start computing the situation for the current step
-	void computeCurrentStep();
+    //! (Re)start (after a stop) the updater if it is not running.
+    void start();
+
+    //! Stop (pause) the updater if it is running (return its exit status).
+    void stop();
+
+    //! Terminate the updater (return its exit status ; wait for the thread to return).
+    int terminate();
+
+    //! Get the situation for the previous step
+    struct RmInfo* getPreviousStep();
+
+    //! Compute 1 situation update step.
+    void runOneStep(double deltaTimeIncrement);
+
+    //! Start computing the situation for the current step
+    void computeCurrentStep();
 
 private:
 
-	//! The thread function.
-	int threadLoop();
-	
-	//! The C wrapper on the thread function.
-	static int threadLoop(void*);
-	
-	//! Acknowledge the situation events (simu / graphics synchronization).
-	void acknowledgeEvents();
+    //! The thread function.
+    int threadLoop();
 
-	//! Allocate and initialize a situation (set constants from source).
-	struct RmInfo* initSituation(const struct RmInfo* pSource);
-	
-	//! (Deep) Copy the given situation.
-	struct RmInfo* copySituation(struct RmInfo*& pTarget, const struct RmInfo* pSource);
+    //! The C wrapper on the thread function.
+    static int threadLoop(void*);
 
-	// Record the situation to a replay database
-	void replaySituation(struct RmInfo*& pTarget);
+    //! Acknowledge the situation events (simu / graphics synchronization).
+    void acknowledgeEvents();
 
-	// Record the situation to a replay database
-	void ghostcarSituation(struct RmInfo*& pTarget);
+    //! Allocate and initialize a situation (set constants from source).
+    struct RmInfo* initSituation(const struct RmInfo* pSource);
 
-	//! Free the given situation
-	void freezSituation(struct RmInfo*& pSituation);
-	
+    //! (Deep) Copy the given situation.
+    struct RmInfo* copySituation(struct RmInfo*& pTarget, const struct RmInfo* pSource);
+
+    // Record the situation to a replay database
+    void replaySituation(struct RmInfo*& pTarget);
+
+    // Record the situation to a replay database
+    void ghostcarSituation(struct RmInfo*& pTarget);
+
+    //! Free the given situation
+    void freezSituation(struct RmInfo*& pSituation);
+
 private:
 
-	//! Initial number of drivers racing
-	int _nInitDrivers;
+    //! Initial number of drivers racing
+    int _nInitDrivers;
 
-	//! The previous step of the situation
-	struct RmInfo* _pPrevReInfo;
+    //! The previous step of the situation
+    struct RmInfo* _pPrevReInfo;
 
-	//! The situation updater thread
-	struct SDL_Thread* _pUpdateThread;
+    //! The situation updater thread
+    struct SDL_Thread* _pUpdateThread;
 
-	//! True if the updater is actually threaded (may be not the case)
-	bool _bThreaded;
+    //! True if the updater is actually threaded (may be not the case)
+    bool _bThreaded;
 
-	//! True if thread affinity has to be applied (even in case !_bThreaded)
-	bool _bThreadAffinity;
+    //! True if thread affinity has to be applied (even in case !_bThreaded)
+    bool _bThreadAffinity;
 
-	//! Flag to set in order to terminate the updater.
-	bool _bTerminate;
+    //! Flag to set in order to terminate the updater.
+    bool _bTerminate;
 
-	//! Simulation tick (s) (defaults to RCM_MAX_DT_SIMU).
-	double _fSimuTick;
+    //! Simulation tick (s) (defaults to RCM_MAX_DT_SIMU).
+    double _fSimuTick;
 
-	//! Output tick (s) : defines the computeCurrentStep output "rate"
-	// - if 0, a nearly real-time rate is enforced, at the expense of variable output rate,
-	//   (used in normal real-time gaming mode)
-	// - if > 0, the output rate is enforced, at the expense of not keeping the real time.
-	//   (used in movie capture gaming mode, to get a stable but slowed-down frame rate).
-	double _fOutputTick;
+    //! Output tick (s) : defines the computeCurrentStep output "rate"
+    // - if 0, a nearly real-time rate is enforced, at the expense of variable output rate,
+    //   (used in normal real-time gaming mode)
+    // - if > 0, the output rate is enforced, at the expense of not keeping the real time.
+    //   (used in movie capture gaming mode, to get a stable but slowed-down frame rate).
+    double _fOutputTick;
 
-	//! Time of the last output when using the "stable but slowed-down frame rate" mode.
-	double _fLastOutputTime;
+    //! Time of the last output when using the "stable but slowed-down frame rate" mode.
+    double _fLastOutputTime;
+
+    //! Gap from real time produced by engine lags.
+    double _fRealTimeGap;
 
 };
 
-#endif /* _RACESITUATION_H_ */ 
+#endif /* _RACESITUATION_H_ */
 
 
 
