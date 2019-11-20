6ac0053f3631a0e8519de620b9f51bebe810a1ab
torcs-ng <torcs-ng@30fe4595-0a0c-4342-8851-515496e4dcbd>
2019-11-20 00:16:25 +0000

    - Update shadow's driver
    git-svn-id: https://svn.code.sf.net/p/speed-dreams/code/trunk@6774 30fe4595-0a0c-4342-8851-515496e4dcbd
    
    Former-commit-id: c04812f7d855e608107b11d93df75ca5c2cd1be6
    Former-commit-id: 92c77b362ab2af286403590e9418cb28ce0e7845

--- src/drivers/shadow/src/WheelModel.cpp
+++ src/drivers/shadow/src/WheelModel.cpp
@@ -1,3 +1,19 @@
+/***************************************************************************
+
+    file        : WheelModel.cpp
+    created     : 02 november 2019
+    copyright   : (C) 2006 Tim Foden - (C) 2019 Xavier Bertaux
+
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
 
 #include <robottools.h>
 
