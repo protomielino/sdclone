a345afc70e4d6d847ddbcb476320bf2dbcde3092
scttgs0 <scttgs0@30fe4595-0a0c-4342-8851-515496e4dcbd>
2020-04-25 16:59:23 +0000

    Suppress Compiler Warnings - Deprecated implicit copy ctor
    git-svn-id: https://svn.code.sf.net/p/speed-dreams/code/trunk@6965 30fe4595-0a0c-4342-8851-515496e4dcbd
    
    Former-commit-id: 0a461ec2e72c9c8a0f6c83b3fafbfa194731f7ad
    Former-commit-id: 655f2489360aae4b3fa2274ea807c84d043dcfb5

--- src/drivers/simplix/src/unitcarparam.h
+++ src/drivers/simplix/src/unitcarparam.h
@@ -8,7 +8,7 @@
 // File         : unitcarparam.h
 // Created      : 2007.11.25
 // Last changed : 2014.11.29
-// Copyright    : © 2007-2014 Wolf-Dieter Beelitz
+// Copyright    : ï¿½ 2007-2014 Wolf-Dieter Beelitz
 // eMail        : wdbee@users.sourceforge.net
 // Version      : 4.05.000
 //--------------------------------------------------------------------------*
@@ -24,17 +24,17 @@
 //
 // Das Programm wurde unter Windows XP entwickelt und getestet.
 // Fehler sind nicht bekannt, dennoch gilt:
-// Wer die Dateien verwendet erkennt an, dass für Fehler, Schäden,
-// Folgefehler oder Folgeschäden keine Haftung übernommen wird.
+// Wer die Dateien verwendet erkennt an, dass fï¿½r Fehler, Schï¿½den,
+// Folgefehler oder Folgeschï¿½den keine Haftung ï¿½bernommen wird.
 //--------------------------------------------------------------------------*
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation; either version 2 of the License, or
 // (at your option) any later version.
 //
-// Im übrigen gilt für die Nutzung und/oder Weitergabe die
+// Im ï¿½brigen gilt fï¿½r die Nutzung und/oder Weitergabe die
 // GNU GPL (General Public License)
-// Version 2 oder nach eigener Wahl eine spätere Version.
+// Version 2 oder nach eigener Wahl eine spï¿½tere Version.
 //--------------------------------------------------------------------------*
 #ifndef _UNITCARPARAM_H_
 #define _UNITCARPARAM_H_
@@ -46,6 +46,7 @@ class TCarParam
 {
   public:
 	TCarParam();                                 // Default constructor
+	TCarParam(const TCarParam&) = default;       // Default copy constructor
 	virtual ~TCarParam();                        // Destructor
 	virtual TCarParam& operator= (const TCarParam& CarParam);
 
