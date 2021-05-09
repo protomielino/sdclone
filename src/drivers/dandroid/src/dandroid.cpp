2705a7a9ebae014f3678461d21e21f247c322fc9
torcs-ng <torcs-ng@30fe4595-0a0c-4342-8851-515496e4dcbd>
2021-05-09 23:48:41 +0000

    - Update dandroid_sc's driver
    git-svn-id: https://svn.code.sf.net/p/speed-dreams/code/trunk@7509 30fe4595-0a0c-4342-8851-515496e4dcbd
    
    Former-commit-id: 6be058814eac4760a00a8dbcbd71ad151ca8ee37
    Former-commit-id: 73eefd314ec309b126663f20b7b5fc0720315397

--- src/drivers/dandroid/src/dandroid.cpp
+++ src/drivers/dandroid/src/dandroid.cpp
@@ -1,19 +1,19 @@
 /***************************************************************************
 
-    file                 : dandroid.cpp
-    created              : 2006-08-31 01:21:49 UTC
-    copyright            : (C) Daniel Schellhammer
+	file                 : dandroid.cpp
+	created              : 2006-08-31 01:21:49 UTC
+	copyright            : (C) Daniel Schellhammer
 
  ***************************************************************************/
 
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
+ /***************************************************************************
+  *                                                                         *
+  *   This program is free software; you can redistribute it and/or modify  *
+  *   it under the terms of the GNU General Public License as published by  *
+  *   the Free Software Foundation; either version 2 of the License, or     *
+  *   (at your option) any later version.                                   *
+  *                                                                         *
+  ***************************************************************************/
 
 #ifdef _WIN32
 #include <windows.h>
@@ -27,7 +27,7 @@
 #include <utility>
 #include "driver.h"
 
-// The "DANDROID" logger instance
+  // The "DANDROID" logger instance
 GfLogger* PLogDANDROID = 0;
 
 using ::std::string;
@@ -35,13 +35,13 @@ using ::std::vector;
 using ::std::pair;
 
 // TORCS interface
-static void initTrack(int index, tTrack* track, void *carHandle, void **carParmHandle, tSituation *s);
-static void newRace(int index, tCarElt* car, tSituation *s);
-static void drive(int index, tCarElt* car, tSituation *s);
-static int pitcmd(int index, tCarElt* car, tSituation *s);
+static void initTrack(int index, tTrack* track, void* carHandle, void** carParmHandle, tSituation* s);
+static void newRace(int index, tCarElt* car, tSituation* s);
+static void drive(int index, tCarElt* car, tSituation* s);
+static int pitcmd(int index, tCarElt* car, tSituation* s);
 static void shutdown(int index);
-static int InitFuncPt(int index, void *pt);
-static void endRace(int index, tCarElt *car, tSituation *s);
+static int InitFuncPt(int index, void* pt);
+static void endRace(int index, tCarElt* car, tSituation* s);
 
 // SD interface
 static const int BUFSIZE = 256;
@@ -62,7 +62,7 @@ static const string defaultBotDesc[MAXNBBOTS] = {  // NOLINT(runtime/string)
 
 // Drivers info: pair(first:Name, second:Desc)
 static vector< pair<string, string> > Drivers;
-static TDriver *driver[MAXNBBOTS];  // Array of drivers
+static TDriver* driver[MAXNBBOTS];  // Array of drivers
 
 // Number of drivers defined in robot's xml-file
 static int NBBOTS = 0;      // Still unknown
@@ -73,19 +73,19 @@ static string pathBuffer;   // Robot's xml-filename // NOLINT(runtime/string)
 static int indexOffset = 0;
 // Marker for undefined drivers to be able to comment out drivers
 // in the robot's xml-file between others, not only at the end of the list
-const char *sUndefined = "undefined";
+const char* sUndefined = "undefined";
 
 ////////////////////////////////
 // Utility
 ////////////////////////////////
 
 // Set robots's name and xml file pathname
-static void setRobotName(const string &name)
+static void setRobotName(const string& name)
 {
-  char buffer[BUFSIZE];
-  snprintf(buffer, BUFSIZE, "drivers/%s/%s.xml", name.c_str(), name.c_str());
-  nameBuffer = name;
-  pathBuffer = buffer;
+	char buffer[BUFSIZE];
+	snprintf(buffer, BUFSIZE, "drivers/%s/%s.xml", name.c_str(), name.c_str());
+	nameBuffer = name;
+	pathBuffer = buffer;
 }
 
 ////////////////////////////////////////////////////////////////
@@ -95,185 +95,198 @@ static void setRobotName(const string &name)
 // Module entry point (new fixed name scheme).
 // Extended for use with schismatic robots
 
-extern "C" int moduleWelcome(const tModWelcomeIn* welcomeIn,
-                              tModWelcomeOut* welcomeOut)
+extern "C" int moduleWelcome(const tModWelcomeIn * welcomeIn,
+	tModWelcomeOut * welcomeOut)
 {
-  // Save module name and loadDir, and determine module XML file pathname.
-  setRobotName(welcomeIn->name);
+	// Save module name and loadDir, and determine module XML file pathname.
+	setRobotName(welcomeIn->name);
 
-  // Filehandle for robot's xml-file
-  void *pRobotSettings = GfParmReadFile(pathBuffer.c_str(), GFPARM_RMODE_STD);
+	// Filehandle for robot's xml-file
+	void* pRobotSettings = GfParmReadFile(pathBuffer.c_str(), GFPARM_RMODE_STD);
 
-  PLogDANDROID = GfLogger::instance("DANDROID");
+	PLogDANDROID = GfLogger::instance("DANDROID");
 
-  if (pRobotSettings) // robot settings XML could be read
-  {
-    NBBOTS = 0;
+	if (pRobotSettings) // robot settings XML could be read
+	{
+		NBBOTS = 0;
 
-    char SectionBuffer[BUFSIZE];
-    snprintf(SectionBuffer, BUFSIZE, "%s/%s/%d", ROB_SECT_ROBOTS, ROB_LIST_INDEX, 0);
+		char SectionBuffer[BUFSIZE];
+		snprintf(SectionBuffer, BUFSIZE, "%s/%s/%d", ROB_SECT_ROBOTS, ROB_LIST_INDEX, 0);
 
-    // Try to get first driver from index 0
-    const string sDriverName = GfParmGetStrNC(pRobotSettings,
-                                    SectionBuffer,
-                                    ROB_ATTR_NAME,
-                                    const_cast<char*>(sUndefined));
+		// Try to get first driver from index 0
+		const string sDriverName = GfParmGetStrNC(pRobotSettings,
+			SectionBuffer,
+			ROB_ATTR_NAME,
+			const_cast<char*>(sUndefined));
 
-    // Check whether index 0 is used as start index
-    if (sDriverName != sUndefined) {
-      // Teams xml file uses index 0, 1, ..., N - 1
-      indexOffset = 0;
-    } else {
-      // Teams xml file uses index 1, 2, ..., N
-      indexOffset = 1;
-    }
+		// Check whether index 0 is used as start index
+		if (sDriverName != sUndefined)
+		{
+			// Teams xml file uses index 0, 1, ..., N - 1
+			indexOffset = 0;
+		}
+		else
+		{
+			// Teams xml file uses index 1, 2, ..., N
+			indexOffset = 1;
+		}
 
-    // Loop over all possible drivers, clear all buffers,
-    // save defined driver names and descriptions.
-    Drivers.clear();
-    for (int i = indexOffset; i < MAXNBBOTS + indexOffset; ++i) {
-      snprintf(SectionBuffer, BUFSIZE, "%s/%s/%d", ROB_SECT_ROBOTS, ROB_LIST_INDEX, i);
+		// Loop over all possible drivers, clear all buffers,
+		// save defined driver names and descriptions.
+		Drivers.clear();
 
-      string sDriverName = GfParmGetStr(pRobotSettings, SectionBuffer,
-                                          ROB_ATTR_NAME, sUndefined);
+		for (int i = indexOffset; i < MAXNBBOTS + indexOffset; ++i)
+		{
+			snprintf(SectionBuffer, BUFSIZE, "%s/%s/%d", ROB_SECT_ROBOTS, ROB_LIST_INDEX, i);
 
-      if (sDriverName != sUndefined) {
-        // This driver is defined in robot's xml-file
-        string sDriverDesc = GfParmGetStr(pRobotSettings, SectionBuffer,
-                                    ROB_ATTR_DESC, defaultBotDesc[i].c_str());
-        Drivers.push_back(make_pair(sDriverName, sDriverDesc));
-        ++NBBOTS;
-      }
-    }  // for i
-    GfParmReleaseHandle(pRobotSettings);
-  } else {  // if robot settings XML could not be read
-    // For schismatic robots NBBOTS is unknown! Handle error here
-    NBBOTS = 0;
-    // But this is not considered a real failure of moduleWelcome !
-  }
+			string sDriverName = GfParmGetStr(pRobotSettings, SectionBuffer,
+				ROB_ATTR_NAME, sUndefined);
 
-  // Set max nb of interfaces to return.
-  welcomeOut->maxNbItf = NBBOTS;
+			if (sDriverName != sUndefined)
+			{
+				// This driver is defined in robot's xml-file
+				string sDriverDesc = GfParmGetStr(pRobotSettings, SectionBuffer,
+					ROB_ATTR_DESC, defaultBotDesc[i].c_str());
+				Drivers.push_back(make_pair(sDriverName, sDriverDesc));
+				++NBBOTS;
+			}
+		}  // for i
 
-  return 0;
+		GfParmReleaseHandle(pRobotSettings);
+	}
+	else
+	{  // if robot settings XML could not be read
+	  // For schismatic robots NBBOTS is unknown! Handle error here
+		NBBOTS = 0;
+		// But this is not considered a real failure of moduleWelcome !
+	}
+
+	// Set max nb of interfaces to return.
+	welcomeOut->maxNbItf = NBBOTS;
+
+	return 0;
 }
 
 // Module entry point (new fixed name scheme).
-extern "C" int moduleInitialize(tModInfo *modInfo)
+extern "C" int moduleInitialize(tModInfo * modInfo)
 {
-  // Clear all structures.
-  memset(modInfo, 0, NBBOTS * sizeof(tModInfo));
-  for (int i = 0; i < NBBOTS; i++) {
+	// Clear all structures.
+	memset(modInfo, 0, NBBOTS * sizeof(tModInfo));
+
+	for (int i = 0; i < NBBOTS; i++)
+	{
 #ifdef DANDROID_TORCS
-    modInfo[i].name = strdup(Drivers[i].first.c_str());
-    modInfo[i].desc = strdup(Drivers[i].second.c_str());
+		modInfo[i].name = strdup(Drivers[i].first.c_str());
+		modInfo[i].desc = strdup(Drivers[i].second.c_str());
 #else
-    modInfo[i].name = Drivers[i].first.c_str();
-    modInfo[i].desc = Drivers[i].second.c_str();
+		modInfo[i].name = Drivers[i].first.c_str();
+		modInfo[i].desc = Drivers[i].second.c_str();
 #endif
-    modInfo[i].fctInit = InitFuncPt;       // Init function.
-    modInfo[i].gfId    = ROB_IDENT;        // Supported framework version.
-    modInfo[i].index   = i + indexOffset;  // Indices from robot's xml-file.
-  }  // for i
+		modInfo[i].fctInit = InitFuncPt;       // Init function.
+		modInfo[i].gfId = ROB_IDENT;        // Supported framework version.
+		modInfo[i].index = i + indexOffset;  // Indices from robot's xml-file.
+	}  // for i
 
-  return 0;
+	return 0;
 }
-
 
 // Module exit point (new fixed name scheme).
-extern "C" int moduleTerminate() {
-  return 0;
+extern "C" int moduleTerminate()
+{
+	return 0;
 }
-
-
 
 ////////////////////////////////////////////////////////////////
 // TORCS backward compatibility scheme, from Andrew's USR code
 ////////////////////////////////////////////////////////////////
 
 // Module entry point
-extern "C" int dandroid(tModInfo *modInfo) {
-  NBBOTS = 10;
-  Drivers.clear();
-  pathBuffer = "drivers/dandroid/dandroid.xml";
-  nameBuffer = "dandroid";
+extern "C" int dandroid(tModInfo * modInfo)
+{
+	NBBOTS = 10;
+	Drivers.clear();
+	pathBuffer = "drivers/dandroid/dandroid.xml";
+	nameBuffer = "dandroid";
 
-  // Filehandle for robot's xml-file
-  void *pRobotSettings = GfParmReadFile(pathBuffer.c_str(), GFPARM_RMODE_STD);
+	// Filehandle for robot's xml-file
+	void* pRobotSettings = GfParmReadFile(pathBuffer.c_str(), GFPARM_RMODE_STD);
 
-  if (pRobotSettings) {  // Let's look what we have to provide here
-    char SectionBuffer[BUFSIZE];
-    for (int i = 0; i < NBBOTS; i++) {
-      snprintf(SectionBuffer, BUFSIZE, "%s/%s/%d", ROB_SECT_ROBOTS, ROB_LIST_INDEX, i);
-      string sDriverName = GfParmGetStr(pRobotSettings, SectionBuffer, ROB_ATTR_NAME, defaultBotName[i].c_str());
-      string sDriverDesc = GfParmGetStr(pRobotSettings, SectionBuffer, ROB_ATTR_DESC, defaultBotDesc[i].c_str());
-      Drivers.push_back(make_pair(sDriverName, sDriverDesc));
-    }
-    GfParmReleaseHandle(pRobotSettings);
-  }
+	if (pRobotSettings)
+	{  // Let's look what we have to provide here
+		char SectionBuffer[BUFSIZE];
 
-  return moduleInitialize(modInfo);
+		for (int i = 0; i < NBBOTS; i++)
+		{
+			snprintf(SectionBuffer, BUFSIZE, "%s/%s/%d", ROB_SECT_ROBOTS, ROB_LIST_INDEX, i);
+			string sDriverName = GfParmGetStr(pRobotSettings, SectionBuffer, ROB_ATTR_NAME, defaultBotName[i].c_str());
+			string sDriverDesc = GfParmGetStr(pRobotSettings, SectionBuffer, ROB_ATTR_DESC, defaultBotDesc[i].c_str());
+			Drivers.push_back(make_pair(sDriverName, sDriverDesc));
+		}
+		GfParmReleaseHandle(pRobotSettings);
+	}
+
+	return moduleInitialize(modInfo);
 }
-
 
 // Module exit point (TORCS backward compatibility scheme).
-extern "C" int dandroidShut() {
-  return moduleTerminate();
+extern "C" int dandroidShut()
+{
+	return moduleTerminate();
 }
-
 
 // Module interface initialization.
-static int InitFuncPt(int index, void *pt) {
-  tRobotItf *itf = static_cast<tRobotItf *>(pt);
+static int InitFuncPt(int index, void* pt)
+{
+	tRobotItf* itf = static_cast<tRobotItf*>(pt);
 
-  // Create robot instance for index.
-  driver[index] = new TDriver(index);
-  driver[index]->MyBotName = nameBuffer.c_str();
+	// Create robot instance for index.
+	driver[index] = new TDriver(index);
+	driver[index]->MyBotName = nameBuffer.c_str();
 
-  itf->rbNewTrack = initTrack;    // Give the robot the track view called.
-  itf->rbNewRace  = newRace;      // Start a new race.
-  itf->rbDrive    = drive;        // Drive during race.
-  itf->rbPitCmd   = pitcmd;       // Pit commands.
-  itf->rbEndRace  = endRace;      // End of the current race.
-  itf->rbShutdown = shutdown;     // Called before the module is unloaded.
-  itf->index      = index;        // Index used if multiple interfaces.
-  return 0;
+	itf->rbNewTrack = initTrack;    // Give the robot the track view called.
+	itf->rbNewRace = newRace;      // Start a new race.
+	itf->rbDrive = drive;        // Drive during race.
+	itf->rbPitCmd = pitcmd;       // Pit commands.
+	itf->rbEndRace = endRace;      // End of the current race.
+	itf->rbShutdown = shutdown;     // Called before the module is unloaded.
+	itf->index = index;        // Index used if multiple interfaces.
+	return 0;
 }
-
 
 // Called for every track change or new race.
-static void initTrack(int index, tTrack* track, void *carHandle,
-                      void **carParmHandle, tSituation *s) {
-  driver[index]->InitTrack(track, carHandle, carParmHandle, s);
+static void initTrack(int index, tTrack* track, void* carHandle,
+	void** carParmHandle, tSituation* s)
+{
+	driver[index]->InitTrack(track, carHandle, carParmHandle, s);
 }
-
 
 // Start a new race.
-static void newRace(int index, tCarElt* car, tSituation *s) {
-  driver[index]->NewRace(car, s);
+static void newRace(int index, tCarElt* car, tSituation* s)
+{
+	driver[index]->NewRace(car, s);
 }
-
 
 // Drive during race.
-static void drive(int index, tCarElt* car, tSituation *s) {
-  driver[index]->Drive();
+static void drive(int index, tCarElt* car, tSituation* s)
+{
+	driver[index]->Drive();
 }
-
 
 // Pitstop callback.
-static int pitcmd(int index, tCarElt* car, tSituation *s) {
-  return driver[index]->PitCmd();
+static int pitcmd(int index, tCarElt* car, tSituation* s)
+{
+	return driver[index]->PitCmd();
 }
-
 
 // End of the current race.
-static void endRace(int index, tCarElt *car, tSituation *s) {
-  driver[index]->EndRace();
+static void endRace(int index, tCarElt* car, tSituation* s)
+{
+	driver[index]->EndRace();
 }
 
-
 // Called before the module is unloaded.
-static void shutdown(int index) {
-  driver[index]->Shutdown();
-  delete driver[index];
+static void shutdown(int index)
+{
+	driver[index]->Shutdown();
+	delete driver[index];
 }
