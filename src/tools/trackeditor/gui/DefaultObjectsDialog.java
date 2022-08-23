7c8fbfbfabaf2db2e6bfcc22deb038bf75ccde7e
iobyte <iobyte@30fe4595-0a0c-4342-8851-515496e4dcbd>
2022-08-23 15:03:32 +0000

    trackeditor: fix default objects dialog size
    git-svn-id: https://svn.code.sf.net/p/speed-dreams/code/trunk@8424 30fe4595-0a0c-4342-8851-515496e4dcbd
    
    Former-commit-id: b3bc36e14cc83e7ad77deeec1fabdacd6e56c197
    Former-commit-id: 57a131c008273190b7b2b43b05b9c4863eaa69f9

--- src/tools/trackeditor/gui/DefaultObjectsDialog.java
+++ src/tools/trackeditor/gui/DefaultObjectsDialog.java
@@ -22,7 +22,7 @@ public class DefaultObjectsDialog extends JDialog
 	private void initialize()
 	{
 		this.setContentPane(getObjectProperties());
-		this.setSize(550, 323);
+		this.setSize(550, 435);
 		Point p = new Point();
 		p.x = editorFrame.getProject().getDefaultObjectsDialogX();
 		p.y = editorFrame.getProject().getDefaultObjectsDialogY();
