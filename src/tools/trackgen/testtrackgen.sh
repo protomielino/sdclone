efc29ba2f65c72eef526b21606a5acbbaa691db6
iobyte <iobyte@30fe4595-0a0c-4342-8851-515496e4dcbd>
2022-09-22 18:14:12 +0000

    fix trackgen e-track-4 regression test
    git-svn-id: https://svn.code.sf.net/p/speed-dreams/code/trunk@8539 30fe4595-0a0c-4342-8851-515496e4dcbd
    
    Former-commit-id: 12708bd3de27b5c72483cf486c6e64ceb267588d
    Former-commit-id: 0feee458f3e8e8d7a90460cb186a412df2273dc0

--- src/tools/trackgen/testtrackgen.sh
+++ src/tools/trackgen/testtrackgen.sh
@@ -8,7 +8,7 @@ temp_dir=$(mktemp -d)
 pwd=`pwd`
 
 check () {
-	$trackgen -c $1 -n $2 -i $pwd/../../../data/tracks/$1/$2 -o $temp_dir -a >& /dev/null
+	$trackgen -c $1 -n $2 $3 -i $pwd/../../../data/tracks/$1/$2 -o $temp_dir -a >& /dev/null
 
 	diff $pwd/../../../data/tracks/$1/$2/$2.ac $temp_dir/$2.ac > /dev/null
 
@@ -22,7 +22,7 @@ check development showroom
 check development straight-10
 check development testtrack
 check dirt mixed-2
-check road e-track-4
+check road e-track-4 -B
 check speedway a-speedway
 
 rm -rf temp_dir
