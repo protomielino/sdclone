d7b00adb77f20d91fe92f856ebe89cbb802d238e
beaglejoe <beaglejoe@30fe4595-0a0c-4342-8851-515496e4dcbd>
2022-09-25 14:05:12 +0000

    Add FindGLM.cmake
    git-svn-id: https://svn.code.sf.net/p/speed-dreams/code/trunk@8546 30fe4595-0a0c-4342-8851-515496e4dcbd
    
    Former-commit-id: 2766a52c29d66a713400502dd97cdba16fcdc3fc
    Former-commit-id: f91aa66c7702a55bd3d9e40795efe22358c29f04

---
+++ cmake/FindGLM.cmake
@@ -0,0 +1,21 @@
+# Locate GLM math (header-only) library
+# This module defines
+# GLM_FOUND : 
+# GLM_INCLUDE_DIR : where to find the headers
+# Created by Joe Thompson
+
+find_path(GLM_INCLUDE_DIR glm/glm.hpp)
+
+
+# handle the QUIETLY and REQUIRED arguments and set OGG_FOUND to TRUE if
+# all listed variables are TRUE
+include(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(GLM DEFAULT_MSG GLM_INCLUDE_DIR)
+
+#if(GLM_FOUND)
+set(GLM_LIBRARIES ${GLM_LIBRARY})
+#endif()
+# Set the old GLM_INCLUDE_DIRS variable for backwards compatibility
+set(GLM_INCLUDE_DIRS ${GLM_INCLUDE_DIR})
+
+mark_as_advanced(GLM_LIBRARY GLM_INCLUDE_DIR)
