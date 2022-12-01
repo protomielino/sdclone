de0c19496f85ad250ca9b02d27fdbd1a5635bb10
iobyte <iobyte@30fe4595-0a0c-4342-8851-515496e4dcbd>
2022-12-01 14:43:37 +0000

    trackeditor: check for relief files that cause trackgen to generate bad terrain
    git-svn-id: https://svn.code.sf.net/p/speed-dreams/code/trunk@8651 30fe4595-0a0c-4342-8851-515496e4dcbd
    
    Former-commit-id: efd9549ac86f58e82b43818448dd625f38a04cce
    Former-commit-id: 1e93d8aa98a638fd31879962136d27dc9a9dd724

--- src/tools/trackeditor/gui/CheckDialog.java
+++ src/tools/trackeditor/gui/CheckDialog.java
@@ -22,6 +22,10 @@ import javax.swing.WindowConstants;
 import utils.Editor;
 import utils.SegmentVector;
 import utils.TrackData;
+import utils.ac3d.Ac3d;
+import utils.ac3d.Ac3dException;
+import utils.ac3d.Ac3dObject;
+import utils.ac3d.Ac3dSurface;
 import utils.circuit.Curve;
 import utils.circuit.EnvironmentMapping;
 import utils.circuit.ObjShapeObject;
@@ -56,11 +60,11 @@ public class CheckDialog extends JDialog
 
 	/**
 	 * This method initializes this
-	 * 
+	 *
 	 * @return void
 	 */
 	private void initialize()
-	{	
+	{
 		this.setTitle("Check Track");
 		this.setSize(editorFrame.getProject().getCheckDialogWidth(), editorFrame.getProject().getCheckDialogHeight());
 		this.setResizable(true);
@@ -68,18 +72,18 @@ public class CheckDialog extends JDialog
 		p.x = editorFrame.getProject().getCheckDialogX();
 		p.y = editorFrame.getProject().getCheckDialogY();
 		this.setLocation(p);
-		
+
 		textArea = new JTextArea();
 	    textArea.setLineWrap(false);
 	    textArea.setEditable(false);
 	    textArea.setVisible(true);
-	    
+
 	    scrollPane = new JScrollPane (textArea);
 	    scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 	    scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-		
+
 	    add(scrollPane);
-	    
+
 	    addComponentListener(new ComponentAdapter()
 	    {
 	    	public void componentShown(ComponentEvent e)
@@ -93,8 +97,8 @@ public class CheckDialog extends JDialog
 	    		checkGraphic();
 	    		checkPits();
 	    		checkTrack();
-			
-	    		textArea.append("Checking complete!");	
+
+	    		textArea.append("Checking complete!");
 	    	}
 	    });
 	    this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
@@ -118,7 +122,7 @@ public class CheckDialog extends JDialog
 				{
 					startWidth += curve.getValidRightBorderWidth(editorFrame);
 					endWidth = startWidth;
-					
+
 					startWidth += curve.getValidRightSideStartWidth(editorFrame);
 					endWidth += curve.getValidRightSideEndWidth(editorFrame);
 				}
@@ -126,7 +130,7 @@ public class CheckDialog extends JDialog
 				{
 					startWidth += curve.getValidLeftBorderWidth(editorFrame);
 					endWidth = startWidth;
-					
+
 					startWidth += curve.getValidLeftSideStartWidth(editorFrame);
 					endWidth += curve.getValidLeftSideEndWidth(editorFrame);
 				}
@@ -194,19 +198,19 @@ public class CheckDialog extends JDialog
 		double firstHeightStartRight = Double.NaN;
 		double previousHeightEndLeft = Double.NaN;
 		double previousHeightEndRight = Double.NaN;
-		
+
 		for (int i = 0; i < segments.size(); i++)
 		{
 			Segment segment = segments.get(i);
-			
+
 			String	segmentInfo = "Segment " + segment.getName() + " : ";
 
 			double heightStartLeft = segment.getHeightStartLeft();
 			boolean hasHeightStartLeft = !Double.isNaN(heightStartLeft);
 
-			double heightStartRight = segment.getHeightStartRight();			
+			double heightStartRight = segment.getHeightStartRight();
 			boolean hasHeightStartRight = !Double.isNaN(heightStartRight);
-			
+
 			boolean hasHeightStart = hasHeightStartLeft && hasHeightStartRight;
 			boolean hasBankingStartFromHeights = hasHeightStart && heightStartLeft != heightStartRight;
 
@@ -225,7 +229,7 @@ public class CheckDialog extends JDialog
 
 					if (centerHeight != 0.0)
 					{
-						// disabled because many tracks don't start at 0 because of banking 
+						// disabled because many tracks don't start at 0 because of banking
 						// or because they use the actual elevation
 						//textArea.append(segmentInfo + "Track height at start should be 0. Actual height: " + centerHeight +"\n");
 					}
@@ -240,14 +244,14 @@ public class CheckDialog extends JDialog
 
 				if (bankingStart != bankingFromHeights)
 				{
-					textArea.append(segmentInfo + "Banking start: " + bankingStart + " doesn't match banking from heights: " + bankingFromHeights + "\n");							
+					textArea.append(segmentInfo + "Banking start: " + bankingStart + " doesn't match banking from heights: " + bankingFromHeights + "\n");
 				}
 			}
 
 			double heightEndLeft = segment.getHeightEndLeft();
 			boolean hasHeightEndLeft = !Double.isNaN(heightEndLeft);
 
-			double heightEndRight = segment.getHeightEndRight();			
+			double heightEndRight = segment.getHeightEndRight();
 			boolean hasHeightEndRight = !Double.isNaN(heightEndRight);
 
 			boolean hasHeightEnd = hasHeightEndLeft && hasHeightEndRight;
@@ -259,12 +263,12 @@ public class CheckDialog extends JDialog
 			if (hasBankingEnd && hasBankingEndFromHeights)
 			{
 				textArea.append(segmentInfo + "Banking end angle and banking from heights\n");
-				
+
 				double bankingFromHeights = Math.atan2(heightEndLeft - heightEndRight, width) * 180.0 / Math.PI;
-		
+
 				if (bankingEnd != bankingFromHeights)
 				{
-					textArea.append(segmentInfo + "Banking end: " + bankingEnd + " doesn't match banking from heights: " + bankingFromHeights + "\n");							
+					textArea.append(segmentInfo + "Banking end: " + bankingEnd + " doesn't match banking from heights: " + bankingFromHeights + "\n");
 				}
 			}
 
@@ -282,17 +286,17 @@ public class CheckDialog extends JDialog
 					textArea.append(segmentInfo + "Grade and end height\n");
 				}
 			}
-			
+
 			Segment previous = segment.previousShape;
 			Segment next = segment.nextShape;
-			
+
 			if (previous != null && hasHeightStart)
 			{
 				if (!Double.isNaN(previousHeightEndLeft) && previousHeightEndLeft != heightStartLeft)
 				{
 					textArea.append(segmentInfo + "Previous height end left : " + previousHeightEndLeft + " doesn't match " + heightStartLeft + "\n");
 				}
-				
+
 				if (!Double.isNaN(previousHeightEndRight) && previousHeightEndRight != heightStartRight)
 				{
 					textArea.append(segmentInfo + "Previous height end right : " + previousHeightEndRight + " doesn't match " + heightStartRight + "\n");
@@ -304,13 +308,13 @@ public class CheckDialog extends JDialog
 				{
 					textArea.append(segmentInfo + "Height end left : " + heightEndLeft + " doesn't match " + firstHeightStartLeft + "\n");
 				}
-				
+
 				if (!Double.isNaN(firstHeightStartRight) && firstHeightStartRight != heightEndRight)
 				{
 					textArea.append(segmentInfo + "Height end right : " + heightEndRight + " doesn't match " + firstHeightStartRight + "\n");
 				}
 			}
-			
+
 			previousHeightEndLeft = heightEndLeft;
 			previousHeightEndRight = heightEndRight;
 		}
@@ -322,7 +326,7 @@ public class CheckDialog extends JDialog
 		if (hasText(image))
 			checkTexture("Graphic Background Image ", image);
 	}
-	
+
 	private void checkTrackLights()
 	{
 		Vector<TrackLight> lightData = trackData.getTrackLights();
@@ -341,14 +345,14 @@ public class CheckDialog extends JDialog
 	private void checkEnvironmentMapping()
 	{
 		Vector<EnvironmentMapping>	envMaps = trackData.getGraphic().getEnvironmentMapping();
-		
+
 		for (int i = 0; i < envMaps.size(); i++)
 		{
 			String texture = envMaps.get(i).getEnvMapImage();
 			checkTexture("Environment Map " + envMaps.get(i).getName(), texture);
 		}
 	}
-	
+
 	private void checkTerrainGeneration()
 	{
 		String reliefFile = trackData.getGraphic().getTerrainGeneration().getReliefFile();
@@ -363,29 +367,62 @@ public class CheckDialog extends JDialog
 			}
 			else
 			{
+				Ac3d ac3dFile = new Ac3d();
+
 				try
 				{
-					BufferedReader br = new BufferedReader(new FileReader(file));
-					String line = "";
+					ac3dFile.read(file);
 
-					while ((line = br.readLine()) != null)
+					Ac3dObject root = ac3dFile.getRoot();
+
+					if (root != null && "world".equals(root.getType()))
 					{
-						if (line.startsWith("data"))
+						for (int i = 0; i < root.getKids().size(); i++)
 						{
-							String name = br.readLine();
+							Ac3dObject object = root.getKids().get(i);
 
-							if (!(name.equals("interior") || name.equals("exterior")))
+							if ("poly".equals(object.getType()))
 							{
-								textArea.append("Terrain Generation relief file " + reliefFile + " invalid\n");
-								break;
+								String data = object.getData();
+
+								if (data == null)
+								{
+									textArea.append("Terrain Generation relief file " + reliefFile + " line " + object.getLinenum() + " : missing interior or exterior data\n");									
+								}
+								else if (!(data.equals("interior") || data.equals("exterior")))
+								{
+									textArea.append("Terrain Generation relief file " + reliefFile + " line " + object.getLinenum() + " : expected interior or exterior data but found " + data + "\n");
+								}
+
+								// sd2-trackgen expects borders and holes to be closed line and generates bad terrain when using line and duplicate vertices 
+								for (int j = 0; j < object.getSurfaces().size(); j++)
+								{
+									Ac3dSurface	surface = object.getSurfaces().get(j);
+
+									if (surface.isLine())
+									{
+										double[] firstVertex = object.getVertices().get(0);
+										double[] lastVertex = object.getVertices().get(object.getVertices().size() - 1);
+										
+										if (firstVertex[0] == lastVertex[0] && firstVertex[1] == lastVertex[1] && firstVertex[2] == lastVertex[2])
+										{
+											textArea.append("Terrain Generation relief file " + reliefFile + " line " + object.getLinenum() + " : line with first and last vertices same\n");																				
+										}
+									}
+								}
 							}
 						}
 					}
-					br.close();
-				} 
+
+					ac3dFile.write(file.toString() + ".ac");
+				}
+				catch (Ac3dException e)
+				{
+					textArea.append("Terrain Generation relief file " + reliefFile + " line " + e.getLineNumber() + " : " + e.getLocalizedMessage() + "\n");
+				}
 				catch (Exception e)
 				{
-					e.printStackTrace();
+					textArea.append("Terrain Generation relief file " + reliefFile + " : " + e.getLocalizedMessage() + "\n");
 				}
 			}
 		}
@@ -583,13 +620,13 @@ public class CheckDialog extends JDialog
 		}
 
 		File file = findObjectFile(object);
-		
+
 		if (file == null)
 		{
 			textArea.append(type + " object " + trackObject.getName() + " model " + object + " not found\n");
 			return;
 		}
-		
+
 		try
 		{
 			BufferedReader br = new BufferedReader(new FileReader(file));
@@ -604,7 +641,7 @@ public class CheckDialog extends JDialog
 		        }
 		    }
 		    br.close();
-		} 
+		}
 		catch (Exception e)
 		{
 			e.printStackTrace();
@@ -629,7 +666,7 @@ public class CheckDialog extends JDialog
 			{
 				messages.add(" texture " + texture + " should be converted to png format\n");
 			}
-			
+
 			if (texture.contains("/") || texture.contains("\\"))
 			{
 				messages.add(" texture " + texture + " should remove path\n");
@@ -646,7 +683,7 @@ public class CheckDialog extends JDialog
 		for (int i = 0; i < trackData.getObjects().size(); i++)
 		{
 			String name = trackData.getObjects().get(i).getName();
-			
+
 			if (name == null || name.isEmpty())
 			{
 				textArea.append("Track object " + (i + 1) + " missing name\n");
@@ -657,7 +694,7 @@ public class CheckDialog extends JDialog
 				{
 					if (name.equals(trackData.getObjects().get(j).getName()))
 					{
-						textArea.append("Track object " + (i + 1) + " " + name + " has same name as Track object " + (j + 1) + " " + trackData.getObjects().get(j).getName() + "\n");						
+						textArea.append("Track object " + (i + 1) + " " + name + " has same name as Track object " + (j + 1) + " " + trackData.getObjects().get(j).getName() + "\n");
 					}
 				}
 
@@ -665,7 +702,7 @@ public class CheckDialog extends JDialog
 				{
 					if (name.equals(defaultObjects.get(j).getName()))
 					{
-						textArea.append("Track object " + (i + 1) + " " + name + " has same name as Default object " + (j + 1) + " " + defaultObjects.get(j).getName() + "\n");						
+						textArea.append("Track object " + (i + 1) + " " + name + " has same name as Default object " + (j + 1) + " " + defaultObjects.get(j).getName() + "\n");
 					}
 				}
 			}
@@ -676,7 +713,7 @@ public class CheckDialog extends JDialog
 		{
 			String name = trackData.getObjects().get(i).getName();
 			int color = trackData.getObjects().get(i).getColor();
-			
+
 			if (color == Integer.MAX_VALUE)
 			{
 				textArea.append("Track object " + (i + 1) + " missing color\n");
@@ -687,7 +724,7 @@ public class CheckDialog extends JDialog
 				{
 					if (color == trackData.getObjects().get(j).getColor())
 					{
-						textArea.append("Track object " + (i + 1) + " " + name + " has same color as Track object " + (j + 1) + " " + trackData.getObjects().get(j).getName() + "\n");						
+						textArea.append("Track object " + (i + 1) + " " + name + " has same color as Track object " + (j + 1) + " " + trackData.getObjects().get(j).getName() + "\n");
 					}
 				}
 
@@ -695,7 +732,7 @@ public class CheckDialog extends JDialog
 				{
 					if (color == defaultObjects.get(j).getColor())
 					{
-						textArea.append("Track object " + (i + 1) + " " + name + " has same color as Default object " + (j + 1) + " " + defaultObjects.get(j).getName() + "\n");						
+						textArea.append("Track object " + (i + 1) + " " + name + " has same color as Default object " + (j + 1) + " " + defaultObjects.get(j).getName() + "\n");
 					}
 				}
 			}
@@ -731,7 +768,7 @@ public class CheckDialog extends JDialog
 	{
 		if (surface == null || surface.isEmpty())
 			return;
-		
+
 		String message = description + " surface " + surface;
 
 		for (int i = 0; i < trackData.getSurfaces().size(); i++)
@@ -770,7 +807,7 @@ public class CheckDialog extends JDialog
 				return;
 			}
 		}
-		
+
 		textArea.append(description + " surface " + surface + " not found\n");
 	}
 	private void checkSurfaces()
@@ -779,7 +816,7 @@ public class CheckDialog extends JDialog
 		for (int i = 0; i < trackData.getSurfaces().size(); i++)
 		{
 			String name = trackData.getSurfaces().get(i).getName();
-			
+
 			if (!hasText(name))
 			{
 				textArea.append("Track surface " + (i + 1) + " missing name\n");
@@ -790,15 +827,15 @@ public class CheckDialog extends JDialog
 				{
 					if (name.equals(trackData.getSurfaces().get(j).getName()))
 					{
-						textArea.append("Track surface " + (i + 1) + " " + name + " has same name as Track surface " + (j + 1) + " " + trackData.getSurfaces().get(i).getName() + "\n");						
+						textArea.append("Track surface " + (i + 1) + " " + name + " has same name as Track surface " + (j + 1) + " " + trackData.getSurfaces().get(i).getName() + "\n");
 					}
 				}
-				
+
 				for (int j = 0; j < defaultSurfaces.size(); j++)
 				{
 					if (name.equals(defaultSurfaces.get(j).getName()))
 					{
-						textArea.append("Track surface " + (i + 1) + " " + name + " has same name as Default surface " + (j + 1) + " " + defaultSurfaces.get(i).getName() + "\n");						
+						textArea.append("Track surface " + (i + 1) + " " + name + " has same name as Default surface " + (j + 1) + " " + defaultSurfaces.get(i).getName() + "\n");
 					}
 				}
 			}
@@ -814,7 +851,7 @@ public class CheckDialog extends JDialog
 		for (int i = 0; i < trackData.getSegments().size(); i++)
 		{
 			Segment segment = trackData.getSegments().get(i);
-			
+
 			checkSurface(segment.getSurface(), "Segment " + segment.getName() + " Track");
 			checkSurface(segment.getLeft().getBorderSurface(), "Segment " + segment.getName() + " Left Border");
 			checkSurface(segment.getLeft().getSideSurface(), "Segment " + segment.getName() + " Left Side");
