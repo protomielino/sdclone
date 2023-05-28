f3a4f88f40371e7b9c81fc343067eaa2ab1c37e0
iobyte <iobyte@30fe4595-0a0c-4342-8851-515496e4dcbd>
2023-05-28 23:05:09 +0000

    trackgen: bump version
    git-svn-id: https://svn.code.sf.net/p/speed-dreams/code/trunk@8952 30fe4595-0a0c-4342-8851-515496e4dcbd
    
    Former-commit-id: 2bae431f76dc1fb79e5ab7e7c9f15d7ab27445f1
    Former-commit-id: e4a07002327eca48100a6899dbee86ee38e9c32f

--- src/tools/trackgen/main.cpp
+++ src/tools/trackgen/main.cpp
@@ -98,7 +98,7 @@ public:
 
 //! Constructor.
 Application::Application()
-: GfApplication("TrackGen", "1.6.0.9", "Terrain generator for tracks")
+: GfApplication("TrackGen", "1.6.0.10", "Terrain generator for tracks")
 , HeightSteps(30)
 , Bump(false)
 , Raceline(false)
