08750d24a7589c00201e41be2db5199f56560226
beaglejoe <beaglejoe@30fe4595-0a0c-4342-8851-515496e4dcbd>
2023-10-11 19:43:24 +0000

    updated SQLite3 to 3.43.2
    git-svn-id: https://svn.code.sf.net/p/speed-dreams/code/trunk@9164 30fe4595-0a0c-4342-8851-515496e4dcbd
    
    Former-commit-id: 2cba92f5c4cbfee055f776911d7a84b761ac31e6
    Former-commit-id: 894dc830f85230a28b113db2431429e480f9d9a1

--- packaging/3rdParty-devel/thirdpartydefinitions.cmake
+++ packaging/3rdParty-devel/thirdpartydefinitions.cmake
@@ -202,11 +202,11 @@ set(OSG_HASH SHA256=0e9e3e4cc6f463f21a901934a95e9264b231a1d5db90f72dcb4b8cc94b0d
 
 # sqlite3
 message(STATUS "Note the YEAR in the path AND hard-coded filename")
-set(SQLITE3_VERSION 3.43.1)
+set(SQLITE3_VERSION 3.43.2)
 set(SQLITE3_PROJECT sqlite3-${SQLITE3_VERSION})
-set(SQLITE3_FILE sqlite-amalgamation-3430100.zip)
+set(SQLITE3_FILE sqlite-amalgamation-3430200.zip)
 set(SQLITE3_URL https://www.sqlite.org/2023/${SQLITE3_FILE})
-set(SQLITE3_HASH SHA256=7e634bbd4b2870a83dc7c1e3cc02e4d30b8555cd7db7b332f24e0c447fd0dd16)
+set(SQLITE3_HASH SHA256=a17ac8792f57266847d57651c5259001d1e4e4b46be96ec0d985c953925b2a1c)
 
 # GLM
 set(GLM_VERSION 0.9.9.8)
