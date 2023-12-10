9190e21fb8d100c1879d3345fa2f34b8d43d6b81
beaglejoe <beaglejoe@30fe4595-0a0c-4342-8851-515496e4dcbd>
2023-12-10 22:10:37 +0000

    Update cURL to 8.5.0
    git-svn-id: https://svn.code.sf.net/p/speed-dreams/code/trunk@9228 30fe4595-0a0c-4342-8851-515496e4dcbd
    
    Former-commit-id: 79f980d34fba57822acd412132e457462c7066b1
    Former-commit-id: 569ffabfbd0f41fecbb4cd0419b6f932e6085431

--- packaging/3rdParty-devel/thirdpartydefinitions.cmake
+++ packaging/3rdParty-devel/thirdpartydefinitions.cmake
@@ -185,13 +185,13 @@ set(FREETYPE_LEGACY_URL https://sourceforge.net/projects/freetype/files/freetype
 set(FREETYPE_LEGACY_HASH SHA256=5c78216d6c5860ef694fde1418d20d69d0ac83ab346c21eb311bd45709e0d93a)
 
 # curl
-set(CURL_VERSION 8.4.0)
+set(CURL_VERSION 8.5.0)
 string(REPLACE "." "_" CURL_TAG ${CURL_VERSION})
 set(CURL_PROJECT curl-${CURL_VERSION})
 set(CURL_FILE ${CURL_PROJECT}.tar.bz2)
 #set(CURL_URL https://curl.se/download/${CURL_FILE})
 set(CURL_URL https://github.com/curl/curl/releases/download/curl-${CURL_TAG}/${CURL_FILE})
-set(CURL_HASH SHA256=e5250581a9c032b1b6ed3cf2f9c114c811fc41881069e9892d115cc73f9e88c6)
+set(CURL_HASH SHA256=ce4b6a6655431147624aaf582632a36fe1ade262d5fab385c60f78942dd8d87b)
 
 # osg
 set(OSG_VERSION 3.6.5)
