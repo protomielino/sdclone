879ed45df6945e4517f57bb36784e222989d0687
iobyte <iobyte@30fe4595-0a0c-4342-8851-515496e4dcbd>
2024-02-01 23:10:45 +0000

    trackeditor: add some functions to Point3D
    git-svn-id: https://svn.code.sf.net/p/speed-dreams/code/trunk@9298 30fe4595-0a0c-4342-8851-515496e4dcbd
    
    Former-commit-id: eefeee6b190fad989d4a8c2157dac5ca96721ea6
    Former-commit-id: a0a7225edb8947c97078ed7f860bf5481d494220

--- src/tools/trackeditor/utils/circuit/Point3D.java
+++ src/tools/trackeditor/utils/circuit/Point3D.java
@@ -17,8 +17,41 @@ public class Point3D extends Point2D.Double
 		this.z = z;
 	}
 
-    public String toString()
-    {
-        return "Point3D["+x+", "+y+", "+z+"]";
-    }
+	public void set(double x, double y, double z)
+	{
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+
+	public void sub(Point3D p0, Point3D p1)
+	{
+		x = p0.x - p1.x;
+		y = p0.y - p1.y;
+		z = p0.z - p1.z;
+	}
+
+	public void cross(Point3D p0, Point3D p1)
+	{
+		x = -p0.z * p1.y + p0.y * p1.z;
+		y =  p0.z * p1.x - p0.x * p1.z;
+		z = -p0.y * p1.x + p0.x * p1.y;
+	}
+
+	public double dot(Point3D p)
+	{
+		return x * p.x + y * p.y + z * p.z;
+	}
+
+	public void scaleAdd(double scale, Point3D p0, Point3D p1)
+	{
+		x = scale * p0.x + p1.x;
+		y = scale * p0.y + p1.y;
+		z = scale * p0.z + p1.z;
+	}
+
+	public String toString()
+	{
+		return "Point3D["+x+", "+y+", "+z+"]";
+	}
 }
