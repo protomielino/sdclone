2e0d19fdf89ba7032783b4e2ae093a3f8c2558f3
iobyte <iobyte@30fe4595-0a0c-4342-8851-515496e4dcbd>
2024-06-29 17:22:47 +0000

    trackgen: check all generated files in all regression tests
    git-svn-id: https://svn.code.sf.net/p/speed-dreams/code/trunk@9465 30fe4595-0a0c-4342-8851-515496e4dcbd
    
    Former-commit-id: ead1245da84ff8fc2433868801340168942b81c5
    Former-commit-id: 67d998b6356b9e31d64e0dd68b2accdcd5f072a6

--- src/tools/trackgen/testtrackgenall.sh
+++ src/tools/trackgen/testtrackgenall.sh
@@ -18,11 +18,27 @@ check () {
 	if [ $status -eq 0 ]; then
 		if  test -f "$pwd/../../../data/tracks/$1/$2/$2.ac"; then
 			diff $pwd/../../../data/tracks/$1/$2/$2.ac $temp_dir/$2.ac > $temp_dir/$2.ac.txt
-			[ $? -eq 0 ] && echo "    $1 $2 GOOD" || echo "    $1 $2 BAD"
+			[ $? -eq 0 ] && echo "    $1/$2.ac GOOD" || echo "    $1/$2.ac BAD"
+			if test -f "$pwd/../../../data/tracks/$1/$2/$2-trk.ac"; then
+				diff $pwd/../../../data/tracks/$1/$2/$2-trk.ac $temp_dir/$2-trk.ac > $temp_dir/$2.ac.diff
+				[ $? -eq 0 ] && echo "    $1/$2-trk.ac GOOD" || echo "    $1/$2-trk.ac BAD"
+			fi
+			if test -f "$pwd/../../../data/tracks/$1/$2/$2-msh.ac"; then
+				diff $pwd/../../../data/tracks/$1/$2/$2-msh.ac $temp_dir/$2-msh.ac > $temp_dir/$2.ac.diff
+				[ $? -eq 0 ] && echo "    $1/$2-msh.ac GOOD" || echo "    $1/$2-msh.ac BAD"
+			fi
+			if test -f "$pwd/../../../data/tracks/$1/$2/$2-obj-1.ac"; then
+				diff $pwd/../../../data/tracks/$1/$2/$2-obj-1.ac $temp_dir/$2-obj-1.ac > $temp_dir/$2.ac.diff
+				[ $? -eq 0 ] && echo "    $1/$2-obj-1.ac GOOD" || echo "    $1/$2-obj-1.ac BAD"
+			fi
+			if test -f "$pwd/../../../data/tracks/$1/$2/$2-obj-2.ac"; then
+				diff $pwd/../../../data/tracks/$1/$2/$2-obj-2.ac $temp_dir/$2-obj-2.ac > $temp_dir/$2.ac.diff
+				[ $? -eq 0 ] && echo "    $1/$2-obj-2.ac GOOD" || echo "    $1/$2-obj-2.ac BAD"
+			fi
 		elif  test -f "$pwd/../../../data/tracks/$1/$2/$2-src.ac"; then
 			echo "    FOUND $2-src.ac"
 			diff $pwd/../../../data/tracks/$1/$2/$2-src.ac $temp_dir/$2.ac > $temp_dir/$2.ac.diff
-			[ $? -eq 0 ] && echo "    $1/$2 GOOD" || echo "    $1/$2 BAD"
+			[ $? -eq 0 ] && echo "    $1/$2.ac GOOD" || echo "    $1/$2.ac BAD"
 		else
 			echo "    SKIPPED no $2.ac file"
 		fi
