6881dcbfec70d1bd598872db29bcf08f47d7797e
Xavier Del Campo Romero <xavi.dcr@tutanota.com>
2025-02-15 23:35:22 +0100

    repomenu.cpp: Add missing license header

--- src/modules/userinterface/legacymenu/mainscreens/repomenu.cpp
+++ src/modules/userinterface/legacymenu/mainscreens/repomenu.cpp
@@ -1,3 +1,13 @@
+/*
+ * Speed Dreams, a free and open source motorsport simulator.
+ * Copyright (C) 2024-2025 Xavier Del Campo Romero
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
 #include "repomenu.h"
 #include "downloadservers.h"
 #include <tgfclient.h>
