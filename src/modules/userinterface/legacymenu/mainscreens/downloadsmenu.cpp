fca65dafeda3f7c92e5dd877dc05acc9e2d78d9f
Xavier Del Campo Romero <xavi.dcr@tutanota.com>
2025-02-19 22:47:55 +0100

    downloadsmenu.xml: Update copyright year

--- src/modules/userinterface/legacymenu/mainscreens/downloadsmenu.cpp
+++ src/modules/userinterface/legacymenu/mainscreens/downloadsmenu.cpp
@@ -1,6 +1,6 @@
 /*
  * Speed Dreams, a free and open source motorsport simulator.
- * Copyright (C) 2024 Xavier Del Campo Romero
+ * Copyright (C) 2024-2025 Xavier Del Campo Romero
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
